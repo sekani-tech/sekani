5fee977210e5d5d049d95092dd683d2a965945f3
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-06 16:53:22 +0100

    Ready for something

---
+++ functions/int_lend_upload.php
@@ -0,0 +1,43 @@
+<?php
+// called database connection
+include("connect.php");
+// user manegement session
+session_start();
+?>
+<?php
+$sessint_id = $_SESSION["int_id"];
+$product_id = $_POST['product_id'];
+$principal_amount = $_POST['principal_amount'];
+$loan_term = $_POST['loan_term'];
+$repay_every = $_POST['repay_every'];
+$interest_rate = $_POST['interest_rate'];
+$disbursement_date = $_POST['disbursement_date'];
+$loan_officer = $_POST['loan_officer'];
+$loan_purpose = $_POST['loan_purpose'];
+$linked_savings_acct = $_POST['linked_savings_acct'];
+$repay_start = $_POST['repay_start'];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+$ = $_POST[''];
+
+$query = "INSERT INTO loan (int_id, ) VALUES ('{$}',)";
+
+$res = mysqli_query($connection, $query);
+
+if ($connection->error) {
+    try {
+        throw new Exception("MYSQL error $connection->error <br> $query ", $mysqli->error);
+    } catch (Exception $e) {
+        echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+        echo n12br($e->getTraceAsString());
+    }
+}
+?>
--- institutions/update_user.php
+++ institutions/update_user.php
@@ -3,6 +3,36 @@
     include("header.php");
 
 ?>
+<?php
+if (isset($_GET["edit"])) {
+  $user_id = $_GET["edit"];
+  $update = true;
+  $person = mysqli_query($connection, "SELECT * FROM staff WHERE user_id='$user_id'");
+
+  if (count([$person]) == 1) {
+    $n = mysqli_fetch_array($person);
+    $staff_id = $n['id'];
+    $user_id = $n['user_id'];
+    $int_name = $n['int_name'];
+    $username = $n['username'];
+    $display_name = $n['display_name'];
+    $email = $n['email'];
+    $first_name = $n['first_name'];
+    $last_name = $n['last_name'];
+    $phone = $n['phone'];
+    $description = $n['description'];
+    $address = $n['address'];
+    $date_joined = $n['date_joined'];
+    $org_role = $n['org_role'];
+    $img = $n['img'];
+    $ut = mysqli_query($connection, "SELECT usertype FROM users WHERE id='$user_id'");
+    if (count([$ut]) == 1) {
+      $j = mysqli_fetch_array($ut);
+      $usertype = $j['usertype'];
+    }
+  }
+}
+?>
 <!-- Content added here -->
     <div class="content">
         <div class="container-fluid">
@@ -17,42 +47,48 @@
                 <div class="card-body">
                   <form>
                     <div class="row">
+                      <div class="col-md-2">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">ID</label>
+                          <input type="text" readonly name="staff_id" value="<?php echo $staff_id; ?>" class="form-control">
+                        </div>
+                      </div>
                       <div class="col-md-5">
                         <div class="form-group">
                           <label class="bmd-label-floating">Institution</label>
-                          <input type="text" class="form-control">
+                          <input type="text" name="int_name" value="<?php echo $int_name; ?>" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Username</label>
-                          <input type="text" class="form-control">
+                          <input type="text" name="username" value="<?php echo $username; ?>" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group">
                           <label class="bmd-label-floating">Display name</label>
-                          <input type="text" class="form-control">
+                          <input type="text" name="display_name" value="<?php echo $display_name; ?>" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Email address</label>
-                          <input type="email" class="form-control">
+                          <input type="email" name="email" value="<?php echo $email; ?>" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-md-6">
                         <div class="form-group">
-                          <label class="bmd-label-floating">Fist Name</label>
-                          <input type="text" class="form-control">
+                          <label class="bmd-label-floating">First Name</label>
+                          <input type="text" name="first_name" value="<?php echo $first_name; ?>" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-6">
                         <div class="form-group">
                           <label class="bmd-label-floating">Last Name</label>
-                          <input type="text" class="form-control">
+                          <input type="text" name="last_name" value="<?php echo $last_name; ?>" class="form-control">
                         </div>
                       </div>
                     </div>
@@ -60,7 +96,7 @@
                       <div class="col-md-12">
                         <div class="form-group">
                           <label class="bmd-label-floating">Address</label>
-                          <input type="text" class="form-control">
+                          <input type="text" name="address" value="<?php echo $address; ?>" class="form-control">
                         </div>
                       </div>
                     </div>
@@ -68,14 +104,14 @@
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Date Joined:</label>
-                          <input type="date" class="form-control">
+                          <input type="date" name="date_joined" value="<?php echo $date_joined; ?>" class="form-control">
                         </div>
                       </div>
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Organization Role:</label>
-                          <select name="" id="" class="form-control">
-                              <option value="">..........</option>
+                          <select name="org_role" id="" class="form-control">
+                              <option value="<?php echo $org_role; ?>"><?php echo $org_role; ?></option>
                               <option value="">Staff</option>
                           </select>
                         </div>
@@ -83,18 +119,28 @@
                       <div class="col-md-4">
                         <div class="form-group">
                           <label class="bmd-label-floating">Phone no</label>
-                          <input type="tel" class="form-control">
+                          <input type="tel" name="phone" value="<?php echo $phone; ?>" class="form-control">
                         </div>
                       </div>
                     </div>
                     <div class="row">
-                      <div class="col-md-12">
+                      <div class="col-md-6">
                         <div class="form-group">
                           <label>Profile Photo</label>
                           <div class="form-group">
                             <label class="bmd-label-floating"> Use .jpg or png files other file types are not acceptible.</label>
-                            <input type="text" placeholder="select image" name="" class="form-control" id="">
+                            <input type="text" name="img" value="<?php echo $img; ?>" placeholder="select image" class="form-control" id="">
                           </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">UserType</label>
+                          <select name="usertype" id="" class="form-control">
+                          <option value="<?php echo $usertype; ?>"><?php echo $usertype; ?></option>
+                            <option value="admin">Admin</option>
+                            <option value="staff">Staff</option>
+                          </select>
                         </div>
                       </div>
                     </div>
