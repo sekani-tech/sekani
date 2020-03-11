e6d4ad80ced79859e8eff8ec651fb0f964b834ea
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-11 20:52:15 +0100

    Done with client upload

--- functions/config.php
+++ functions/config.php
@@ -1,17 +1,17 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_CHARSET', 'utf8');
-define('DB_NAME', 'sekani_admin');
-// test
 // define('DB_SERVER', 'localhost');
 // define('DB_USERNAME', 'root');
-// define('DB_PASSWORD', 'password');
+// define('DB_PASSWORD', '');
 // define('DB_CHARSET', 'utf8');
 // define('DB_NAME', 'sekani_admin');
+// test
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'password');
+define('DB_CHARSET', 'utf8');
+define('DB_NAME', 'sekani_admin');
 
 // MUTE NOTICES
 error_reporting(E_ALL & ~E_NOTICE);
