ad16563dcb1e72fa6f9ec4196f4c857a4bba88b3
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-12 21:54:11 +0100

    loan

--- functions/int_lend_upload.php
+++ functions/int_lend_upload.php
@@ -3,7 +3,7 @@ include("connect.php");
 session_start();
 
 $sessint_id = $_SESSION["int_id"];
-$client_id = $_SESSION["client_id"];
+$client_id = $_POST["client_id"];
 $goacctn = mysqli_query($connection, "SELECT account_no FROM client WHERE id = '$client_id' ");
 if (count([$accnq]) == 1) {
     $a = mysqli_fetch_array($accnq);
@@ -21,7 +21,8 @@ $linked_savings_acct = $_POST['linked_savings_acct'];
 $repay_start = $_POST['repay_start'];
 // part for collatera
 $col_id = $_POST['col_id'];
-$col_name = $_POST['col_description'];
+$col_name = $_POST['col_name'];
+$col_description = $_POST['col_description'];
 // gaurantors
 $first_name = $_POST['gau_first_name'];
 $last_name = $_POST['gau_last_name'];
@@ -31,9 +32,29 @@ $home_address = $_POST['gau_home_address'];
 $office_address = $_POST['gau_office_address'];
 $position_held = $_POST['gau_position_held'];
 $email = $_POST['gau_email'];
+// date of submitted
+// lc
+$r = $interest_rate;
+$prina = $principal_amount;
+$gi = $r * $prina;
+$pd = $gi + $prina;
 
-$query = "INSERT INTO loan (int_id, ) VALUES ('{$}',)";
-
+$submitted_on = date("Y-m-d");
+$currency = "NGN";
+$cd = 2;
+$query = "INSERT INTO loan (int_id, account_no, client_id,
+product_id, col_id, col_name, col_description,
+loan_officer, loan_purpose, currency_code,
+currency_digits, principal_amount_proposed, principal_amount,
+loan_term, interest_rate, approved_principal, repayment_date,
+term_frequency, repay_every, number_of_repayments, submittedon_date,
+submittedon_userid, approvedon_date, approved_userid,
+expected_disburedon_date, expected_firstrepaymenton_date, disbursement_date,
+disbursedon_userid,  ) VALUES ('{$sessint_id}', '{$accr_no}', '{$client_id}',
+'{$product_id}', '{$col_id}', '{$col_name}', '{$col_description}',
+'{$loan_officer}', '{$loan_purpose}', '{$currency}', '{$cd}',
+'{$principal_amount}', '{$pd}')";
+// stopped at principal amount
 $res = mysqli_query($connection, $query);
 
 if ($connection->error) {
