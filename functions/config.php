6f2347c22e900875b0efc48e21812249bcbb3bcc
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-20 11:55:50 +0100

    alright for config

--- functions/config.php
+++ functions/config.php
@@ -6,6 +6,7 @@ define('DB_USERNAME', 'sekanisy');
 define('DB_PASSWORD', '4r6WY#JP+rnl67');
 define('DB_CHARSET', 'utf8');
 define('DB_NAME', 'sekani_admin');
+// hello
 // test
 // define('DB_SERVER', 'localhost');
 // define('DB_USERNAME', 'root');
