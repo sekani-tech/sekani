75b41f3b969a090d1940e47fafd07bbdffb1f1bb
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-27 14:38:34 +0100

    alright here we goo

--- mfi/transact.php
+++ mfi/transact.php
@@ -11,78 +11,78 @@ include("header.php");
         <div class="container-fluid">
           <!-- your content here -->
           <div class="row">
-          <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $runaccount = mysqli_query($connection, "SELECT * FROM account WHERE account_no ='$_POST['account_no']' || account_no='$_POST['account_no2']' && int_id = '$sessint_id' ");
-    if (count([$runaccount]) == 1) {
-        $x = mysqli_fetch_array($runaccount);
-        $brnid = $x['branch_id'];
-        $product_id = $x['product_id'];
-        $acct_b_d = $x['account_balance_derived'];
-        $client_id = $x['client_id'];
-        // test product id - logic COMING SOON
-        // test for d/w
-        $test = $_POST['test'];
-        $acct_no = $_POST['account_no'];
-        $amt = $_POST['amount'];
-        $type = $_POST['pay_type'];
-        // sec
-        $test2 = $_POST['test2'];
-        $acct_no2 = $_POST['account_no2'];
-        $amt2 = $_POST['amount2'];
-        $type2 = $_POST['pay_type2'];
-        if ($test == "deposit") {
-            // calculate the new balance
-            $trancache = "INSERT INTO `transact_cache` (`int_id`, `account_no`, `client_id`, `amount`, `pay_type`, `transact_type`, `product_type`, `status`) VALUES
-            ('{$sessint_id}', '{$acct_no}', '{$client_id}', '{$amt}', '{$type}', 'Deposit', '{$product_id}', 'Not Verified') ";
-            if ($trancache) {
-              echo "<script>".swal({ title:"Done!", text: "Deposit Has Been Done, Awaiting Approval!", type: "success", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
-            } else {
-                echo "<script>".swal({ title:"Error!", text: "Transaction Failed!", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
-                // throw an error of why
-                if ($connection->error) {
-                    try {
-                        throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
-                    } catch (Exception $e) {
-                        echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
-                        echo n12br($e->getTraceAsString());
-                    }
-            }
-            }
-        } else if ($test == "withdraw") {
-           if ($acct_b_d >= $amt2) {
-            $trancache = "INSERT INTO `transact_cache` (`int_id`, `account_no`, `client_id`, `amount`, `pay_type`, `transact_type`, `product_type`, `status`) VALUES
-            ('{$sessint_id}', '{$acct_no2}', '{$client_id}', '{$amt2}', '{$type2}', 'Withdrawal', '{$product_id}', 'Not Verified') ";
-            if ($trancache) {
-              echo "<script>".swal({ title:"Done!", text: "Withdrawal Has Been Done, Awaiting Approval!", type: "success", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
-            } else {
-                echo "<script>".swal({ title:"Error!", text: "Transaction Failed!", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
-                // throw an error of why
-                if ($connection->error) {
-                    try {
-                        throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
-                    } catch (Exception $e) {
-                        echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
-                        echo n12br($e->getTraceAsString());
-                    }
-            }
-            }
-           } else {
-            echo "<script>".swal({ title:"Error!", text: "Insufficient Fund", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
-           }
-        } else {
-            echo "Test is Empty";
-        }
-    }
-    if ($connection->error) {
-            try {
-                throw new Exception("MYSQL error $connection->error <br> $runaccount ", $mysqli->error);
-            } catch (Exception $e) {
-                echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
-                echo n12br($e->getTraceAsString());
-            }
-    }
-}
+<?php
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     $runaccount = mysqli_query($connection, "SELECT * FROM account WHERE account_no ='$_POST['account_no']' || account_no='$_POST['account_no2']' && int_id = '$sessint_id' ");
+//     if (count([$runaccount]) == 1) {
+//         $x = mysqli_fetch_array($runaccount);
+//         $brnid = $x['branch_id'];
+//         $product_id = $x['product_id'];
+//         $acct_b_d = $x['account_balance_derived'];
+//         $client_id = $x['client_id'];
+
+
+//         $test = $_POST['test'];
+//         $acct_no = $_POST['account_no'];
+//         $amt = $_POST['amount'];
+//         $type = $_POST['pay_type'];
+
+//         $test2 = $_POST['test2'];
+//         $acct_no2 = $_POST['account_no2'];
+//         $amt2 = $_POST['amount2'];
+//         $type2 = $_POST['pay_type2'];
+//         if ($test == "deposit") {
+
+//             $trancache = "INSERT INTO `transact_cache` (`int_id`, `account_no`, `client_id`, `amount`, `pay_type`, `transact_type`, `product_type`, `status`) VALUES
+//             ('{$sessint_id}', '{$acct_no}', '{$client_id}', '{$amt}', '{$type}', 'Deposit', '{$product_id}', 'Not Verified') ";
+//             if ($trancache) {
+//               echo "<script>".swal({ title:"Done!", text: "Deposit Has Been Done, Awaiting Approval!", type: "success", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
+//             } else {
+//                 echo "<script>".swal({ title:"Error!", text: "Transaction Failed!", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
+
+//                 if ($connection->error) {
+//                     try {
+//                         throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
+//                     } catch (Exception $e) {
+//                         echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+//                         echo n12br($e->getTraceAsString());
+//                     }
+//             }
+//             }
+//         } else if ($test == "withdraw") {
+//            if ($acct_b_d >= $amt2) {
+//             $trancache = "INSERT INTO `transact_cache` (`int_id`, `account_no`, `client_id`, `amount`, `pay_type`, `transact_type`, `product_type`, `status`) VALUES
+//             ('{$sessint_id}', '{$acct_no2}', '{$client_id}', '{$amt2}', '{$type2}', 'Withdrawal', '{$product_id}', 'Not Verified') ";
+//             if ($trancache) {
+//               echo "<script>".swal({ title:"Done!", text: "Withdrawal Has Been Done, Awaiting Approval!", type: "success", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
+//             } else {
+//                 echo "<script>".swal({ title:"Error!", text: "Transaction Failed!", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
+
+//                 if ($connection->error) {
+//                     try {
+//                         throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
+//                     } catch (Exception $e) {
+//                         echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+//                         echo n12br($e->getTraceAsString());
+//                     }
+//             }
+//             }
+//            } else {
+//             echo "<script>".swal({ title:"Error!", text: "Insufficient Fund", type: "error", buttonsStyling: false, confirmButtonClass: "btn btn-success"})."<script>";
+//            }
+//         } else {
+//             echo "Test is Empty";
+//         }
+//     }
+//     if ($connection->error) {
+//             try {
+//                 throw new Exception("MYSQL error $connection->error <br> $runaccount ", $mysqli->error);
+//             } catch (Exception $e) {
+//                 echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+//                 echo n12br($e->getTraceAsString());
+//             }
+//     }
+// }
 ?>
               <div class="col-md-12">
                   <div class="card">
