8d1d214f88466dc1a6ff6dd24ab1401446bcd598
flash code <47109193+flash-code1@users.noreply.github.com>
2020-03-27 22:16:31 +0100

    asdfghj

--- functions/deposittrans.php
+++ functions/deposittrans.php
@@ -12,38 +12,41 @@ $type = $_POST['pay_type'];
      if (count([$runaccount]) == 1) {
          $x = mysqli_fetch_array($runaccount);
          $brnid = $x['branch_id'];
+         $tryacc = $x['account_no'];
          $product_id = $x['product_id'];
          $acct_b_d = $x['account_balance_derived'];
          $client_id = $x['client_id'];
          $digits = 6;
          $randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
  
-         if ($test == "deposit") {
-             $dd = "Deposit";
-             $ogs = "Not Verified";
-             $trancache = "INSERT INTO transact_cache (int_id, account_no, client_id, amount, pay_type, transact_type, product_type, status) VALUES ('{$sessint_id}', '{$acct_no}', '{$client_id}', '{$amt}', '{$type}', '{$dd}', '{$product_id}', '{$ogs}')";
-             if ($trancache) {
-               $_SESSION["Lack_of_intfund_$randms"] = "Deposit Has Been Done, Awaiting Approval!";
-                echo header ("Location: ../mfi/transact.php?message=$randms");
-             } else {
-                $_SESSION["Lack_of_intfund_$randms"] = "Transaction Failed";
-                echo header ("Location: ../mfi/transact.php?message2=$randms");
-            //      if ($connection->error) {
-            //          try {
-            //              throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
-            //          } catch (Exception $e) {
-            //              echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
-            //              echo n12br($e->getTraceAsString());
-            //          }
-            //  }
-             }
+         if ($acct_no == $tryacc) {
+            if ($test == "deposit") {
+                $dd = "Deposit";
+                $ogs = "Not Verified";
+                $trancache = "INSERT INTO transact_cache (int_id, account_no, client_id, amount, pay_type, transact_type, product_type, status) VALUES ('{$sessint_id}', '{$acct_no}', '{$client_id}', '{$amt}', '{$type}', '{$dd}', '{$product_id}', '{$ogs}')";
+                if ($trancache) {
+                  $_SESSION["Lack_of_intfund_$randms"] = "Deposit Has Been Done, Awaiting Approval!";
+                   echo header ("Location: ../mfi/transact.php?message=$randms");
+                } else {
+                   $_SESSION["Lack_of_intfund_$randms"] = "Transaction Failed";
+                   echo header ("Location: ../mfi/transact.php?message2=$randms");
+               //      if ($connection->error) {
+               //          try {
+               //              throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
+               //          } catch (Exception $e) {
+               //              echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+               //              echo n12br($e->getTraceAsString());
+               //          }
+               //  }
+                }
+            } else {
+                echo "Not equal to deposit";
+            }
          } else {
-             echo "Not equal to deposit";
+            $_SESSION["Lack_of_intfund_$randms"] = "Account not Found";
+            echo header ("Location: ../mfi/transact.php?message7=$randms");
          }
-    } else {
-        $_SESSION["Lack_of_intfund_$randms"] = "Account not Found";
-        echo header ("Location: ../mfi/transact.php?message7=$randms");
-     }
+    }
 //      if ($connection->error) {
 //              try {
 //                  throw new Exception("MYSQL error $connection->error <br> $runaccount ", $mysqli->error);
--- functions/withdrawtrans.php
+++ functions/withdrawtrans.php
@@ -8,46 +8,53 @@ $test2 = $_POST['test2'];
 $acct_no2 = $_POST['account_no2'];
 $amt2 = $_POST['amount2'];
 $type2 = $_POST['pay_type2'];
+$randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+
  $runaccount = mysqli_query($connection, "SELECT * FROM account WHERE account_no='$acct_no2' && int_id = '$sessint_id' ");
      if (count([$runaccount]) == 1) {
          $x = mysqli_fetch_array($runaccount);
          $brnid = $x['branch_id'];
+         $tryacc = $x['account_no'];
          $product_id = $x['product_id'];
          $acct_b_d = $x['account_balance_derived'];
          $client_id = $x['client_id'];
- 
-         if ($test2 == "withdraw") {
-            if ($acct_b_d >= $amt2) {
-                $wd = "Withdrawal";
-                $gms = "Not Verified";
-             $trancache = "INSERT INTO transact_cache (int_id, account_no, client_id, amount, pay_type, transact_type, product_type, status) VALUES
-             ('{$sessint_id}', '{$acct_no2}', '{$client_id}', '{$amt2}', '{$type2}', '{$gms}', '{$product_id}', '{$gms}') ";
-             if ($trancache) {
-                $_SESSION["Lack_of_intfund_$randms"] = "Withdrawal Successful!";
-               echo header ("Location: ../mfi/transact.php?message3=$randms");
+
+         if ($acct_no2 == $tryacc) {
+            if ($test2 == "withdraw") {
+                if ($acct_b_d >= $amt2) {
+                    $wd = "Withdrawal";
+                    $gms = "Not Verified";
+                 $trancache = "INSERT INTO transact_cache (int_id, account_no, client_id, amount, pay_type, transact_type, product_type, status) VALUES
+                 ('{$sessint_id}', '{$acct_no2}', '{$client_id}', '{$amt2}', '{$type2}', '{$gms}', '{$product_id}', '{$gms}') ";
+                 if ($trancache) {
+                    $_SESSION["Lack_of_intfund_$randms"] = "Withdrawal Successful!";
+                   echo header ("Location: ../mfi/transact.php?message3=$randms");
+                 } else {
+                    $_SESSION["Lack_of_intfund_$randms"] = "Withdrawal Failed";
+                   echo header ("Location: ../mfi/transact.php?message4=$randms");
+     
+                //      if ($connection->error) {
+                //          try {
+                //              throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
+                //          } catch (Exception $e) {
+                //              echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
+                //              echo n12br($e->getTraceAsString());
+                //          }
+                //  }
+                 }
+                } else {
+                    $_SESSION["Lack_of_intfund_$randms"] = "Failed - Insufficient Fund";
+                    header ("Location: ../mfi/transact.php?message5=$randms");
+                }
              } else {
-                $_SESSION["Lack_of_intfund_$randms"] = "Withdrawal Failed";
-               echo header ("Location: ../mfi/transact.php?message4=$randms");
- 
-            //      if ($connection->error) {
-            //          try {
-            //              throw new Exception("MYSQL error $connection->error <br> $trancache ", $mysqli->error);
-            //          } catch (Exception $e) {
-            //              echo "Error No: ".$e->getCode()." - ".$e->getMessage() . "<br>";
-            //              echo n12br($e->getTraceAsString());
-            //          }
-            //  }
+                 echo "Test is Empty";
              }
-            } else {
-                $_SESSION["Lack_of_intfund_$randms"] = "Failed - Insufficient Fund";
-                header ("Location: ../mfi/transact.php?message5=$randms");
-            }
          } else {
-             echo "Test is Empty";
+            $_SESSION["Lack_of_intfund_$randms"] = "Account Not Found";
+            echo header ("Location: ../mfi/transact.php?message7=$randms");
          }
-     } else {
-        $_SESSION["Lack_of_intfund_$randms"] = "Account Not Found";
-        echo header ("Location: ../mfi/transact.php?message7=$randms");
+ 
+         
      }
     //  if ($connection->error) {
     //          try {
