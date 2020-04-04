1060c462b22edf0e9757c1c4b4e6bd224b500372
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-04 18:02:00 +0100

    check it out

--- mfi/transact.php
+++ mfi/transact.php
@@ -140,7 +140,7 @@ if (isset($_GET["message"])) {
                                   </div>
                                   <script>
                             $(document).ready(function() {
-                              $('#axt_no').change(function(){
+                              $('#axt_no').keyup(function(){
                                 var id = $(this).val();
                                 var int_id = $sessint_id;
                                 $.ajax({
