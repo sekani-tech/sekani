6bd2944c6bcd65f8049ed2fcbee97cc10c9dcdfa
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-04 20:56:14 +0100

    push lets test blind progress

---
+++ functions/approveTrans.php
@@ -0,0 +1,103 @@
+<?php
+include("connect.php");
+session_start();
+?>
+<?php
+if (isset($_GET['approve']) && $_GET['approve'] !== '') {
+    $appod = $_GET['approve'];
+    $checkm = mysqli_query($connection, "SELECT * FROM transact_cache WHERE id = '$appod'");
+    if (count([$checkm]) == 1) {
+        $x = mysqli_fetch_array($checkm);
+        $ssint_id = $_SESSION["int_id"];
+        $appuser_id = $_SESSION["user_id"];
+        $acct_no = $x['account_no'];
+        $staff_id = $x['staff_id'];
+        $amount = $x['amount'];
+        $pay_type = $x['pay_type'];
+        $transact_type = $x['transact_type'];
+        $product_type = $x['product_type'];
+        $stat = $x['status'];
+        $gen_date = date("Y-m-d");
+        $digits = 4;
+        $randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+        $transid = $ssint_id."-".$randms;
+
+        if ($stat == "Not Verified") {
+            $getacct = mysqli_query($connection, "SELECT * FROM account WHERE account_no = '$acct_no'");
+            if (count([$getacct]) == 1) {
+               $y = mysqli_fetch_array($getacct);
+               $branch_id = $y['branch_id'];
+               $acct_no = $y['account_no'];
+               $client_id = $y['client_id'];
+               $int_acct_bal = $x['account_balance_derived'];
+               $trans_type = "credit";
+               $trans_type2 = "debit";
+               $irvs = 0;
+    
+            //    account deposite computation
+              if($transact_type == "Deposit") {
+                $new_abd = $amount + $int_acct_bal;
+                $iupq = "UPDATE account SET account_balance_derived = '$new_abd',
+                total_deposits_derived = '$amount' WHERE account_no = '$acct_no'";
+                $iupqres = mysqli_query($connection, $iupq);
+                if ($iupqres) {
+                    $iat = "INSERT INTO account_transaction (int_id, branch_id,
+                    account_no, product_id,
+                    client_id, transaction_id, transaction_type, is_reversed,
+                    transaction_date, amount, running_balance_derived, overdraft_amount_derived,
+                    created_date, appuser_id) VALUES ('{$ssint_id}', '{$branch_id}',
+                    '{$acct_no}', '{$product_type}', '{$client_id}', '{$trans_id}', '{$trans_type}', '{$irvs}',
+                    '{$gen_date}', '{$amount}', '{$new_abd}', '{$amount}',
+                    '{$gen_date}', '{$appuser_id}')";
+                    $res3 = mysqli_query($connection, $iat);
+                    if ($res3) {
+                        $_SESSION["Lack_of_intfund_$randms"] = "Successfully Approved";
+                        echo header ("Location: ../mfi/transact_approval.php?message1=$randms");
+                    } else {
+                        $_SESSION["Lack_of_intfund_$randms"] = "Error in Transaction";
+                        echo header ("Location: ../mfi/transact_approval.php?message2=$randms");
+                    }
+                } else {
+                    $_SESSION["Lack_of_intfund_$randms"] = "Error in Account";
+                        echo header ("Location: ../mfi/transact_approval.php?message2=$randms");
+                }
+              } else if ($transact_type == "Withdrawal") {
+                $new_abd = $int_acct_bal - $amount;
+                $iupq = "UPDATE account SET account_balance_derived = '$new_abd',
+                total_withdrawals_derived  = '$amount' WHERE account_no = '$acct_no'";
+                $iupqres = mysqli_query($connection, $iupq);
+                if ($iupqres) {
+                    $iat = "INSERT INTO account_transaction (int_id, branch_id,
+                    account_no, product_id,
+                    client_id, transaction_id, transaction_type, is_reversed,
+                    transaction_date, amount, running_balance_derived, overdraft_amount_derived,
+                    created_date, appuser_id) VALUES ('{$ssint_id}', '{$branch_id}',
+                    '{$acct_no}', '{$product_type}', '{$client_id}', '{$trans_id}', '{$trans_type2}', '{$irvs}',
+                    '{$gen_date}', '{$amount}', '{$new_abd}', '{$amount}',
+                    '{$gen_date}', '{$appuser_id}')";
+                    $res3 = mysqli_query($connection, $iat);
+                    if ($res3) {
+                        $_SESSION["Lack_of_intfund_$randms"] = "Successfully Approved";
+                        echo header ("Location: ../mfi/transact_approval.php?message1=$randms");
+                    } else {
+                        $_SESSION["Lack_of_intfund_$randms"] = "Error in Transaction";
+                        echo header ("Location: ../mfi/transact_approval.php?message2=$randms");
+                    }
+                } else {
+                    $_SESSION["Lack_of_intfund_$randms"] = "Error in Account";
+                        echo header ("Location: ../mfi/transact_approval.php?message2=$randms");
+                }
+              } else {
+                $_SESSION["Lack_of_intfund_$randms"] = "Error No With or Dep";
+                echo header ("Location: ../mfi/transact_approval.php?message2=$randms");
+               }
+
+            }
+        } else {
+            // a message
+            $_SESSION["Lack_of_intfund_$randms"] = "Transaction Has Been Verified Already";
+                echo header ("Location: ../mfi/transact_approval.php?message3=$randms");
+        }
+    }
+}
+?>
--- mfi/transact_approval.php
+++ mfi/transact_approval.php
@@ -3,7 +3,57 @@
     include("header.php");
 
 ?>
-
+<?php
+if (isset($_GET["message1"])) {
+  $key = $_GET["message1"];
+  // $out = $_SESSION["lack_of_intfund_$key"];
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "success",
+          title: "Success",
+          text: "Transaction Successfully Approved",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = null;
+} else if (isset($_GET["message2"])) {
+  $key = $_GET["message2"];
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "error",
+          title: "Error",
+          text: "'.$out = $_SESSION["lack_of_intfund_$key"].'",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = null;
+} else if (isset($_GET["message3"])) {
+  $key = $_GET["message2"];
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "error",
+          title: "Error",
+          text: "'.$out = $_SESSION["lack_of_intfund_$key"].'",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = null;
+} else {
+  echo "";
+}
+?>
 <!-- <link href="vendor/css/addons/datatables.min.css" rel="stylesheet">
 <script type="text/javascript" src="vendor/js/addons/datatables.min.js"></script> -->
 <!-- Content added here -->
@@ -64,7 +114,7 @@
                           <th><?php echo $row["account_off_name"]; ?></th>
                           <th><?php echo $row["client_name"]; ?></th>
                           <th><?php echo $row["status"]; ?></th>
-                          <td><a href="#.php?approve=<?php echo $row["id"];?>" class="btn btn-info">Approve</a></td>
+                          <td><a href="./functions/approveTrans.php?approve=<?php echo $row["id"];?>" class="btn btn-info">Approve</a></td>
                           </tr>
                           <!-- <th></th> -->
                           <?php }
