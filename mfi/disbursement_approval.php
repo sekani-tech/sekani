1fa0c716a5f58fbb0fe61da0b946d3ef564b33f6
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-06 14:25:12 +0100

    alright loan post

---
+++ mfi/disbursement_approval.php
@@ -0,0 +1,148 @@
+<?php
+
+    include("header.php");
+
+?>
+<?php
+if (isset($_GET["message1"])) {
+  $key = $_GET["message1"];
+  $tt = 0;
+  if ($tt !== $_SESSION["lack_of_intfund_$key"]) {
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "success",
+          title: "Success",
+          text: "Transaction Successfully Approved",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = 0;
+ }
+} else if (isset($_GET["message2"])) {
+  $key = $_GET["message2"];
+  $tt = 0;
+  if ($tt !== $_SESSION["lack_of_intfund_$key"]) {
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "error",
+          title: "Error",
+          text: "'.$out = $_SESSION["lack_of_intfund_$key"].'",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = 0;
+}
+} else if (isset($_GET["message3"])) {
+  $key = $_GET["message2"];
+  $tt = 0;
+  if ($tt !== $_SESSION["lack_of_intfund_$key"]) {
+  echo '<script type="text/javascript">
+  $(document).ready(function(){
+      swal({
+          type: "error",
+          title: "Error",
+          text: "'.$out = $_SESSION["lack_of_intfund_$key"].'",
+          showConfirmButton: false,
+          timer: 2000
+      })
+  });
+  </script>
+  ';
+  $_SESSION["lack_of_intfund_$key"] = 0;
+}
+} else {
+  echo "";
+}
+?>
+<!-- <link href="vendor/css/addons/datatables.min.css" rel="stylesheet">
+<script type="text/javascript" src="vendor/js/addons/datatables.min.js"></script> -->
+<!-- Content added here -->
+    <div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">Users</h4>
+                  <script>
+                  $(document).ready(function() {
+                  $('#tabledat').DataTable();
+                  });
+                  </script>
+                  <!-- Insert number users institutions -->
+                  <p class="card-category"><?php
+                   $query = "SELECT * FROM loan_disbursement_cache WHERE int_id='$sessint_id'";
+                   $result = mysqli_query($connection, $query);
+                   if ($result) {
+                     $inr = mysqli_num_rows($result);
+                     echo $inr;
+                   }?> Disbursement Approval on the platform || Approve Loan</p>
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                    <table id="tabledat" class="table" cellspacing="0" style="width:100%">
+                      <thead class=" text-primary">
+                      <?php
+                        $query = "SELECT * FROM loan_disbursement_cache WHERE int_id = '$sessint_id'";
+                        $result = mysqli_query($connection, $query);
+                      ?>
+                        <!-- <th>
+                          ID
+                        </th> -->
+                        <tr>
+                        <th class="th-sm">
+                          Name
+                        </th>
+                        <th class="th-sm">
+                          Principal
+                        </th>
+                        <th class="th-sm">
+                          Interest Value
+                        </th>
+                        <th class="th-sm">Status</th>
+                        <th>Approval</th>
+                        </tr>
+                        <!-- <th>Phone</th> -->
+                      </thead>
+                      <tbody>
+                      <?php if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {?>
+                        <tr>
+                        <?php $row["id"]; ?>
+                          <th><?php echo $row["display_name"]; ?></th>
+                          <th><?php echo $row["principal_amount"]; ?></th>
+                          <th><?php echo $row["interest_rate"]; ?></th>
+                          <th><?php echo $row["status"]; ?></th>
+                          <td><a href="../functions/approveLoan.php?approve=<?php echo $row["id"];?>" class="btn btn-info">Approve</a></td>
+                          </tr>
+                          <!-- <th></th> -->
+                          <?php }
+                          }
+                          else {
+                            echo "0 Loan Approval";
+                          }
+                          ?>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+<?php
+
+    include("footer.php");
+
+?>
