76cd9e7b3ddf8aa8f79dd425ad5d20dac6a6d8a7
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-21 15:35:32 +0100

    tothenext

--- mfi/update_client.php
+++ mfi/update_client.php
@@ -289,7 +289,7 @@ if(isset($_GET["edit"])) {
                         <div class="form-check form-check-inline">
                           <label class="form-check-label">
                               <input class="form-check-input" type="checkbox" value="<?php echo $sms_active;?>" name="sms_active" id="sms">
-                              <input type="text" hidden value="<?php echo $sms_active;?>" id="opo" readonly>
+                              <input type="text" hidden value="<?php echo $sms_active;?>" id="opo">
                               SMS
                               <span class="form-check-sign">
                                 <span class="check"></span>
@@ -299,7 +299,7 @@ if(isset($_GET["edit"])) {
                         <div class="form-check form-check-inline">
                           <label class="form-check-label">
                               <input class="form-check-input" type="checkbox" value="<?php echo $email_active;?>" name="email_active" id="eml">
-                              <input type="text" hidden value="<?php echo $email_active;?>" id="opo2" readonly>
+                              <input type="text" hidden value="<?php echo $email_active;?>" id="opo2">
                               Email
                               <span class="form-check-sign">
                                 <span class="check"></span>
