215e22c29d04e25995a480e8d6aaae37c403c94f
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-24 17:53:05 +0100

    i think i am done

--- change_password.php
+++ change_password.php
@@ -124,7 +124,7 @@ if(isset($_POST["button1"])){
                     </div>
                 </div>
                 <div class="justify-content-center">
-                    <button type="submit" class="btn btn-primary btn-link btn-wd btn-lg">Change Password</button>
+                    <button type="submit" name="button1" value="button1" class="btn btn-primary btn-link btn-wd btn-lg">Change Password</button>
                 </div>
             </div>
     </form>
