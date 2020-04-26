4e9a15ce407c613d758e97e5f44da54497bdfe33
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-26 20:06:07 +0100

    make me push

--- mfi/header.php
+++ mfi/header.php
@@ -46,9 +46,6 @@ if (mysqli_num_rows($getip) == 1) {
      session_destroy();
      $URL="../ip/block_ip.php";
      echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
-  } else {
-      $newcode = $vm + 1;
-      $mmm = mysqli_query($connection, "UPDATE ip_blacklist SET trial = '$newcode' WHERE ip_add = '$ip'");
   }
 }
 ?>
