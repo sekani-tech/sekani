f2db8418a6c3751c001d43f576ed76800e06ba47
flash code <47109193+flash-code1@users.noreply.github.com>
2020-04-30 13:20:04 +0100

    check

--- mfi/ajax_post/chart_acct_post.php
+++ mfi/ajax_post/chart_acct_post.php
@@ -10,17 +10,16 @@ if (isset($_POST["gl"]) && isset($_POST["ch"]) && $_POST["gl"] == "1")
         $acct_use = $_POST["ch"];
             $int_id = $_SESSION["int_id"];
             $org = "SELECT * FROM `acc_gl_account` WHERE int_id = '$int_id' && classification_enum = '$acct_use' && parent_id IS NULL";
-            $res = mysqli_query($connection, $org);
-            while ($row = mysqli_fetch_array($res))
-              {
-                $output2 = '<label >GL Group</label>
+    }
+    $res = mysqli_query($connection, $org);
+      while ($row = mysqli_fetch_array($res))
+        {
+         $output2 = '<label >GL Group</label>
           <select class="form-control" name="parent_id" id="pid">
             <option value="0">choose group</option>
             <option value="'.$row["id"].'">'.$row["name"].'</option>
           </select>';
-              }
-            return $out;
-    echo $output2;
     }
+    echo $output2;
 }
 ?>
