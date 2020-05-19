f5cfb950d693b68eef62c715c8a85cd55c6e431e
H3SAN <46991842+H3SAN@users.noreply.github.com>
2020-05-20 00:59:40 +0100

    Client registration

--- functions/institution_client_upload.php
+++ functions/institution_client_upload.php
@@ -15,15 +15,19 @@ $ekaniE = $_SESSION["sek_email"];
 ?>
 <?php
 $sessint_id = $_SESSION["int_id"];
-$loan_officer_id = $_POST["acct_of"];
 $ctype = strtoupper($_POST['ctype']);
+
+if($ctype == 'INDIVIDUAL')
+{
+  $loan_officer_id = $_POST["acct_of"];
+
 $acct_type = strtoupper($_POST['acct_type']);
 $branch = strtoupper($_POST['branch']);
 $display_name = strtoupper($_POST['display_name']);
 // an account number generation
- $inttest = str_pad($branch, 4, '0', STR_PAD_LEFT);
-$digits = 6;
-$randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+ $inttest = str_pad($branch, 3, '0', STR_PAD_LEFT);
+$digit = 4;
+$randms = str_pad(rand(0, pow(10, $digit)-1), 7, '0', STR_PAD_LEFT);
 $account_no = $inttest."".$randms;
 // auto calculation for the account number generation
 $first_name = strtoupper($_POST['firstname']);
@@ -61,7 +65,7 @@ if ( isset($_POST['email_active']) ) {
     $email_active = 0;
 }
 
-$digits = 10;
+$digits = 9;
 
 $temp = explode(".", $_FILES['signature']['name']);
 $randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
@@ -72,8 +76,6 @@ if (move_uploaded_file($_FILES['signature']['tmp_name'], "clients/sign/" . $imag
 } else {
   $msg = "Image Failed";
 }
-// $image2 = $_FILES['idimg']['name'];
-// $target2 = "clients/".basename($image2);
 
 $temp2 = explode(".", $_FILES['id_img_url']['name']);
 $randms2 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
@@ -85,9 +87,6 @@ $msg = "Image uploaded successfully";
 $msg = "Image Failed";
 }
 
-// $image3 = $_FILES['passport']['name'];
-// $target3 = "clients/".basename($image3);
-
 $temp3 = explode(".", $_FILES['passport']['name']);
 $randms3 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
 $image3 = $randms3. '.' .end($temp3);
@@ -97,30 +96,6 @@ $msg = "Image uploaded successfully";
 } else {
 $msg = "Image Failed";
 }
-// $image1 = $_FILES['signature']['name'];
-// $target1 = "clients/".basename($image1);
-
-// $image2 = $_FILES['idimg']['name'];
-// $target2 = "clients/".basename($image2);
-
-// $image3 = $_FILES['passport']['name'];
-// $target3 = "clients/".basename($image3);
-
-// if (move_uploaded_file($_FILES['signature']['tmp_name'], $target1)) {
-//     $msg = "Image uploaded successfully";
-// }else{
-//     $msg = "Failed to upload image";
-// }
-// if (move_uploaded_file($_FILES['idimg']['tmp_name'], $target2)) {
-//     $msg = "Image uploaded successfully";
-// }else{
-//     $msg = "Failed to upload image";
-// }
-// if (move_uploaded_file($_FILES['passport']['tmp_name'], $target3)) {
-//     $msg = "Image uploaded successfully";
-// }else{
-//     $msg = "Failed to upload image";
-// }
 // gaurantors part
 $query = "INSERT INTO client (int_id, loan_officer_id, client_type, account_type,
 display_name, account_no,
@@ -973,4 +948,251 @@ if ($connection->error) {
             echo nl2br($e->getTraceAsString());
         }
     }
+}
+elseif($ctype == 'CORPORATE')
+{
+  $rc_number = $_POST['rc_number'];
+  $loan_officer_id = $_POST["acct_ofa"];
+  $acct_type = strtoupper($_POST['acct_type']);
+  $branch = strtoupper($_POST['brancha']);
+  $display_name = strtoupper($_POST['display_namea']);
+  $email = $_POST['emaila'];
+  $address = $_POST['addressa'];
+  $date_of_birth = $_POST['date_of_birtha'];
+// an account number generation
+ $inttest = str_pad($branch, 4, '0', STR_PAD_LEFT);
+$digits = 6;
+$randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$account_no = $inttest."".$randms;
+$country = "NIGERIA";
+$loan_status = "Not Active";
+$activation_date = date("Y-m-d");
+$submitted_on = date("Y-m-d");
+$sig_one = $_POST['sig_one'];
+$sig_two = $_POST['sig_two'];
+$sig_three = $_POST['sig_three'];
+$sig_address_one = $_POST['sig_address_one'];
+$sig_address_two = $_POST['sig_address_two'];
+$sig_address_three = $_POST['sig_address_three'];
+$sig_phone_one = $_POST['sig_phone_one'];
+$sig_phone_two = $_POST['sig_phone_two'];
+$sig_phone_three = $_POST['sig_phone_three'];
+$sig_gender_one = $_POST['sig_gender_one'];
+$sig_gender_two = $_POST['sig_gender_two'];
+$sig_gender_three = $_POST['sig_gender_three'];
+$sig_state_one = $_POST['sig_state_one'];
+$sig_state_two = $_POST['sig_state_two'];
+$sig_state_three = $_POST['sig_state_three'];
+$sig_lga_one = $_POST['sig_lga_one'];
+$sig_lga_two = $_POST['sig_lga_two'];
+$sig_lga_three = $_POST['sig_lga_three'];
+$sig_bvn_one = $_POST['sig_bvn_one'];
+$sig_bvn_two = $_POST['sig_bvn_two'];
+$sig_bvn_three = $_POST['sig_bvn_three'];
+
+if( $_POST['sms_active_one']){
+  $sms_active_one = 1;
+}
+else{
+  $sms_active_one = 0;
+}
+if( $_POST['sms_active_two']){
+  $sms_active_two = 1;
+}
+else{
+  $sms_active_two = 0;
+}
+if( $_POST['sms_active_three']){
+  $sms_active_three = 1;
+}
+else{
+  $sms_active_three = 0;
+}
+
+if( $_POST['email_active_one']){
+  $email_active_one = 1;
+}
+else{
+  $email_active_one = 0;
+}
+if( $_POST['email_active_two']){
+  $email_active_two = 1;
+}
+else{
+  $email_active_two = 0;
+}
+if( $_POST['email_active_three']){
+  $email_active_three = 1;
+}
+else{
+  $email_active_three = 0;
+}
+
+
+$digits = 7;
+
+$temp1 = explode(".", $_FILES['sig_passport_one']['name']);
+$randms1 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_passport_one = $randms1. '.' .end($temp1);
+if (move_uploaded_file($_FILES['sig_passport_one']['tmp_name'], "clients/passport/" . $sig_passport_one)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp2 = explode(".", $_FILES['sig_passport_two']['name']);
+$randms2 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_passport_two = $randms2. '.' .end($temp2);
+
+if (move_uploaded_file($_FILES['sig_passport_two']['tmp_name'], "clients/passport/" . $sig_passport_two)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp3 = explode(".", $_FILES['sig_passport_three']['name']);
+$randms3 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_passport_three = $randms3. '.' .end($temp3);
+
+if (move_uploaded_file($_FILES['sig_passport_three']['tmp_name'], "clients/passport/" . $sig_passport_three)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp4 = explode(".", $_FILES['sig_signature_one']['name']);
+$randms4 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_signature_one = $randms4. '.' .end($temp4);
+if (move_uploaded_file($_FILES['sig_signature_one']['tmp_name'], "clients/sign/" . $sig_signature_one)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp5 = explode(".", $_FILES['sig_signature_two']['name']);
+$randms5 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_signature_two = $randms5. '.' .end($temp5);
+
+if (move_uploaded_file($_FILES['sig_signature_two']['tmp_name'], "clients/sign/" . $sig_signature_two)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp6 = explode(".", $_FILES['sig_signature_three']['name']);
+$randms6 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_signature_three = $randms6. '.' .end($temp6);
+
+if (move_uploaded_file($_FILES['sig_signature_three']['tmp_name'], "clients/sign/" . $sig_signature_three)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp7 = explode(".", $_FILES['sig_id_img_one']['name']);
+$randms7 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_id_img_one = $randms7. '.' .end($temp7);
+
+if (move_uploaded_file($_FILES['sig_id_img_one']['tmp_name'], "clients/id/" . $sig_id_img_one)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp8= explode(".", $_FILES['sig_id_img_two']['name']);
+$randms8 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_id_img_two = $randms8. '.' .end($temp8);
+
+if (move_uploaded_file($_FILES['sig_id_img_two']['tmp_name'], "clients/id/" . $sig_id_img_two)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$temp9 = explode(".", $_FILES['sig_id_img_three']['name']);
+$randms9 = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
+$sig_id_img_three = $randms9. '.' .end($temp9);
+
+if (move_uploaded_file($_FILES['sig_id_img_three']['tmp_name'], "clients/id/" . $sig_id_img_three)) {
+    $msg = "Image uploaded successfully";
+} else {
+  $msg = "Image Failed";
+}
+
+$sig_id_card_one = $_POST['sig_id_card_one'];
+$sig_id_card_two = $_POST['sig_id_card_two'];
+$sig_id_card_three = $_POST['sig_id_card_three'];
+
+
+$query = "INSERT INTO client  (int_id, loan_officer_id, loan_status, branch_id, client_type, account_no, account_type, activation_date,
+ firstname, display_name, date_of_birth, submittedon_date, email_address, ADDRESS, COUNTRY,
+   rc_number, sig_one, sig_two, sig_three, sig_address_one, sig_address_two, sig_address_three, sig_phone_one, sig_phone_two, sig_phone_three,
+    sig_gender_one, sig_gender_two, sig_gender_three, sig_state_one, sig_state_two, sig_state_three, sig_lga_one, sig_lga_two, sig_lga_three,
+     sig_bvn_one, sig_bvn_two, sig_bvn_three, sms_active_one, sms_active_two, sms_active_three, email_active_one, email_active_two,
+      email_active_three, sig_passport_one, sig_passport_two, sig_passport_three, sig_signature_one, sig_signature_two, sig_signature_three,
+       sig_id_img_one, sig_id_img_two, sig_id_img_three, sig_id_card_one, sig_id_card_two, sig_id_card_three) 
+  VALUES ('{$sessint_id}', '{$loan_officer_id}', '{$loan_status}', '{$branch}', '{$ctype}', '{$account_no}','{$acct_type}', '{$activation_date}', '{$display_name}', '{$display_name}', '{$date_of_birth}',
+  '{$submitted_on}', '{$email}', '{$address}','{$country}', '{$rc_number}','{$sig_one}','{$sig_two}','{$sig_three}','{$sig_address_one}','{$sig_address_two}','{$sig_address_three}',
+  '{$sig_phone_one}','{$sig_phone_two}','{$sig_phone_three}','{$sig_gender_one}','{$sig_gender_two}','{$sig_gender_three}','{$sig_state_one}','{$sig_state_two}','{$sig_state_three}',
+  '{$sig_lga_one}','{$sig_lga_two}','{$sig_lga_three}','{$sig_bvn_one}','{$sig_bvn_two}','{$sig_bvn_three}','{$sms_active_one}','{$sms_active_two}','{$sms_active_three}',
+  '{$email_active_one}','{$email_active_two}','{$email_active_three}','{$sig_passport_one}','{$sig_passport_two}','{$sig_passport_three}','{$sig_signature_one}','{$sig_signature_two}',
+  '{$sig_signature_three}','{$sig_id_img_one}','{$sig_id_img_two}','{$sig_id_img_three}','{$sig_id_card_one}','{$sig_id_card_two}','{$sig_id_card_three}')";
+
+$res = mysqli_query($connection, $query);
+
+ if ($res) {
+    $acctquery = mysqli_query($connection, "SELECT * FROM client WHERE account_no = '$account_no'");
+    if (count([$acctquery]) == 1) {
+        $x = mysqli_fetch_array($acctquery);
+        $int_id = $x['int_id'];
+        $branch_id = $x['branch_id'];
+        $account_no = $x['account_no'];
+        $account_type = $x['account_type'];
+        $client_id = $x['id'];
+        $field_officer_id = $x['loan_officer_id'];
+        $submittedon_date = $x['submittedon_date'];
+        $submittedon_userid = $x['loan_officer_id'];
+        $currency_code = "NGN";
+        $activation_date = $x['activation_date'];
+        $activation_userid = $x['loan_officer_id'];
+        $account_balance_derived = 0;
+
+        $accountins = "INSERT INTO account (int_id, branch_id, account_no, account_type,
+        product_id, client_id, field_officer_id, submittedon_date, submittedon_userid,
+        currency_code, activatedon_date, activatedon_userid,
+        account_balance_derived) VALUES ('{$int_id}', '{$branch_id}', '{$account_no}',
+        '{$accttname}', '{$account_type}', '{$client_id}', '{$field_officer_id}', '{$submittedon_date}',
+        '{$submittedon_userid}', '{$currency_code}', '{$activation_date}', '{$activation_userid}',
+        '{$account_balance_derived}')";
+
+        $go = mysqli_query($connection, $accountins);
+        if ($go) {
+          $_SESSION["Lack_of_intfund_$randms"] = "Registration Successful!";
+          echo header ("Location: ../mfi/client.php?message3=$randms");
+        } else {
+           $_SESSION["Lack_of_intfund_$randms"] = "Registration Failed";
+           echo "error";
+          echo header ("Location: ../mfi/client.php?message4=$randms");
+            // echo header("location: ../mfi/client.php");
+        }
+    }
+
+ } else {
+     echo "<p>Error</p>";
+ }
+if ($connection->error) {
+        try {   
+            throw new Exception("MySQL error $connection->error <br> Query:<br> $query", $mysqli->error);   
+        } catch(Exception $e ) {
+            echo "Error No: ".$e->getCode(). " - ". $e->getMessage() . "<br >";
+            echo nl2br($e->getTraceAsString());
+        }
+    }
+}
+else {
+  $_SESSION["Lack_of_intfund_$randms"] = "Registration Failed";
+  echo "error";
+ echo header ("Location: ../mfi/client.php?message5=$randms");
+   // echo header("location: ../mfi/client.php");
+}
 ?>
--- mfi/manage_client.php
+++ mfi/manage_client.php
@@ -46,233 +46,34 @@ include("header.php");
                   <div class="col-md-4">
                     <div class="form-group">
                       <label>Client Type</label>
-                      <select required name="ctype" class="form-control" id="collat">
-                          <option value="Individual">Individual</option>
+                      <select required name="ctype" class="form-control" id="tom">
+                          <option value="">select</option>
+                          <option value="Individual">Individual Account</option>
                           <option value="Joint">Joint Account</option>
                           <option value="Corporate">Corporate Account</option>
                         </select>
                     </div>
                   </div>
-                  
-                  <!-- </div> -->
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Display name</label>
-                      <input required type="text" required style="text-transform: uppercase;" class="form-control" name="display_name">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >First Name</label>
-                      <input required type="text" style="text-transform: uppercase;" class="form-control" name="firstname">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Middle Name</label>
-                      <input required type="text" style="text-transform: uppercase;" class="form-control" name="middlename">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Last Name</label>
-                      <input required type="text" style="text-transform: uppercase;" class="form-control" name="lastname">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Phone No</label>
-                      <input required type="tel" class="form-control" name="phone">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Phone No2</label>
-                      <input type="tel" class="form-control" name="phone2">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Email address</label>
-                      <input required type="email" class="form-control" name="email">
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="form-group">
-                      <label >Address</label>
-                      <input type="text" style="text-transform: uppercase;" class="form-control" name="address">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label >Gender:</label>
-                      <select required class="form-control" name="gender" id="">
-                        <option value="MALE">MALE</option>
-                        <option value="FEMALE">FEMALE</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label class="">Date of Birth:</label>
-                      <input required type="date" class="form-control" name="date_of_birth">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                  <?php
-                  function fill_branch($connection)
-                  {
-                  $sint_id = $_SESSION["int_id"];
-                  $org = "SELECT * FROM branch WHERE int_id = '$sint_id'";
-                  $res = mysqli_query($connection, $org);
-                  $out = '';
-                  while ($row = mysqli_fetch_array($res))
-                  {
-                    $out .= '<option value="'.$row["id"].'">'.$row["name"].'</option>';
-                  }
-                  return $out;
-                  }
-                  ?>
-                    <div class="form-group">
-                      <label class="">Branch:</label>
-                      <select required name="branch" class="form-control " id="collat">
-                          <option value="">select a Branch</option>
-                          <?php echo fill_branch($connection); ?>
-                        </select>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="">Country:</label>
-                      <input type="text" style="text-transform: uppercase;" class="form-control" value = "NIGERIA" name="country">
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                    <div class="form-group">
-                      <label for="">State:</label>
-                      <select class="form-control " style="text-transform: uppercase;" name="state" id="selState" onchange="configureDropDownLists()">
-                      </select>
-                      
-                    </div>
-                  </div>
-                  <!-- <div class="col-md-4">
-                    <label for="">State:</label>
-                    <input type="text"  class="form-control" id="">
-                  </div> -->
-                  <div class="col-md-4">
-                    <label for="">LGA:</label>
-                      <select  class="form-control" style="text-transform: uppercase;" name="lga" id="selCity">
-                      </select>
-                  </div>
-                  <div class="col-md-4">
-                    <label for="">BVN:</label>
-                    <input required type="text" style="text-transform: uppercase;" name="bvn" class="form-control" id="">
-                  </div>
-                  <div class="col-md-4">
-                    <p><label for="">Active Alerts:</label></p>
-                    <div class="form-check form-check-inline">
-                      <label class="form-check-label">
-                          <input class="form-check-input" name="sms_active" type="checkbox" value="1">
-                          SMS
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                      </label>
-                    </div>
-                    <div class="form-check form-check-inline">
-                      <label class="form-check-label">
-                          <input class="form-check-input" name="email_active" type="checkbox" value="">
-                          Email
-                          <span class="form-check-sign">
-                            <span class="check"></span>
-                          </span>
-                      </label>
-                    </div>
-                  </div>
-                  <div class="col-md-4">
-                  <?php
-                  function fill_officer($connection)
-                  {
-                  $sint_id = $_SESSION["int_id"];
-                  $org = "SELECT * FROM staff WHERE int_id = '$sint_id' ORDER BY staff.display_name ASC";
-                  $res = mysqli_query($connection, $org);
-                  $out = '';
-                  while ($row = mysqli_fetch_array($res))
-                  {
-                    $out .= '<option value="'.$row["id"].'">' .$row["display_name"]. '</option>';
-                  }
-                  return $out;
-                  }
-                  ?>
-                    <div class="form-group">
-                      <label for="">Account Officer:</label>
-                      <select required name="acct_of" class="form-control" id="">
-                        <option value="">select account officer</option>
-                        <?php echo fill_officer($connection); ?>
-                      </select>
-                    </div>
-                  </div>
-                  <style>
-                        input[type="file"]{
-                          display: none;
-                        }
-                        .custom-file-upload{
-                          border: 1px solid #ccc;
-                          display: inline-block;
-                          padding: 6px 12px;
-                          cursor: pointer;
-                        }
-                      </style>
-                      
-                      <div class="col-md-4">
-                    <label for="file-upload" class="btn btn-fab btn-round btn-primary"><i class="material-icons">attach_file</i></label>
-                    <input id ="file-upload" name="passport" type="file" class="inputFileHidden"/>
-                    <label> Select Passport</label>
-                    <div id="file-upload-filename"></div>
-                     </div>
-                    
-                    <div class="col-md-4">
-                    <label for="file-insert" class="btn btn-fab btn-round btn-primary"><i class="material-icons">attach_file</i></label>
-                    <input id ="file-insert" name="signature" type="file" class="inputFileHidden"/>
-                    <label> Select Signature</label>
-                    <div id="iup"></div>
-                    </div>
-                    <script>
-                      var changeq = document.getElementById( 'file-insert' );
-                      var check = document.getElementById( 'iup' );
-                      changeq.addEventListener( 'change', showme );
-                      function showme( event ) {
-                        var one = event.srcElement;
-                        var fname = one.files[0].name;
-                        check.textContent = 'File name: ' + fname;
-                      }
-                    </script>
-                    <div class="col-md-4">
-                    <label for="file-enter" class="btn btn-fab btn-round btn-primary"><i class="material-icons">attach_file</i></label>
-                    <input id ="file-enter" type="file" name="id_img_url" class="inputFileHidden"/>
-                    <label id="rated"> Select ID</label>
-                    <div id="rated"></div>
-                    <script>
-                      var changeq1 = document.getElementById( 'file-enter' );
-                      var check1 = document.getElementById( 'rated' );
-                      changeq1.addEventListener( 'change', showme1 );
-                      function showme1( event ) {
-                        var one1 = event.srcElement;
-                        var fname1 = one1.files[0].name;
-                        check1.textContent = 'File name: ' + fname1;
-                      }
-                    </script>
-                    </div>
-                  <div class="col-md-4">
-                    <label for="">Id Type</label>
-                    <select required name="id_card" class="form-control " id="">
-                      <option value="National ID">National ID</option>
-                      <option value="Voters ID">Voters ID</option>
-                      <option value="International Passport">International Passport</option>
-                      <!-- <option value="Drivers Liscense"></option> -->
-                    </select>
-                  </div>
+                  <script>
+                    $(document).ready(function() {
+                      $('#tom').on("change", function(){
+                        var id = $(this).val();
+                        $.ajax({
+                          url:"ajax_post/client_type.php",
+                          method:"POST",
+                          data:{id:id},
+                          success:function(data){
+                            $('#client').html(data);
+                          }
+                        })
+                      });
+                    });
+                </script>
+                 
                 </div>
+                <div id="client">
+                    
+                    </div>
                 <a href="client.php" class="btn btn-danger">Back</a>
                 <button type="submit" name="submit" id="submit" class="btn btn-primary pull-right">Create Client</button>
                 <div class="clearfix"></div>
