c13eeed00507e4b22c40dc9cff1b58e022b5c38b
H3SAN <46991842+H3SAN@users.noreply.github.com>
2020-05-21 12:27:26 +0100

    Vault Backend changes

--- functions/vaulttrans.php
+++ functions/vaulttrans.php
@@ -1,8 +1,17 @@
 <?php
 include("connect.php");
 session_start();
+require_once "../bat/phpmailer/PHPMailerAutoload.php";
 ?>
+
 <?php
+$int_name = $_SESSION["int_name"];
+$int_email = $_SESSION["int_email"];
+$int_web = $_SESSION["int_web"];
+$int_phone = $_SESSION["int_phone"];
+$int_logo = $_SESSION["int_logo"];
+$int_address = $_SESSION["int_address"];
+$sessint_id = $_SESSION["int_id"];
 $digits = 6;
 $randms = str_pad(rand(0, pow(10, $digits)-1), $digits, '0', STR_PAD_LEFT);
 $sint_id = $_SESSION['int_id'];
@@ -22,6 +31,7 @@ if (isset($_POST['transact_id']) && isset($_POST['type'])) {
     $rock = mysqli_query($connection, $que);
     $yn = mysqli_fetch_array($rock);
     $tellbalance = $yn['account_balance_derived'];
+    $tellname = $yn['descrption'];
     $transdate = date('Y-m-d');
     $crdate = date('Y-m-d H:m:s');
     $vault = mysqli_query($connection, "SELECT * FROM int_vault WHERE branch_id = '$branchid' && int_id = '$sint_id'");
@@ -48,10 +58,108 @@ if (isset($_POST['transact_id']) && isset($_POST['type'])) {
                         '{$tid}', '{$amount}')";
                        $rin = mysqli_query($connection, $record);
                     if($rin){
-                        $_SESSION["Lack_of_intfund_$randms"] = "";
-
-                        echo "error";
-                        echo header ("Location: ../mfi/teller_journal.php?message1=$randms");
+                        // echo a successful message
+            $mail = new PHPMailer;
+            $mail->From = $int_email;
+            $mail->FromName = $int_name;
+            $mail->addAddress('hesanmal316@gmail.com');
+            $mail->addReplyTo($int_email, "Reply");
+            $mail->isHTML(true);
+            $mail->Subject = "Transaction Alert from $int_name";
+            $mail->Body = "<!doctype html>
+            <html lang='en'>
+              <head>
+                <!-- Required meta tags -->
+                <meta charset='utf-8'>
+                <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+                <!-- Bootstrap CSS -->
+                <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>
+            
+                <title>Transaction Alert</title>
+              </head>
+              <body>
+                <div class='container'>
+                    <div class='row justify-content-md-center'>
+                      <div class='col col-lg-6'>
+                        <div class='shadow p-3 mb-5 bg-white rounded'>
+                            <!-- int logo -->
+                            <div class='row justify-content-md-center'>
+                                <img src='$int_logo' height='60px' width='60px' alt='int image' class='rounded mx-auto d-block'>
+                                <div class='spinner-grow text-primary' role='status'>
+                                    <span class='sr-only'>Loading...</span>
+                                  </div>
+                            </div>
+                            <span> <b>$int_name</b> </span> || <span class='lead' style='font-size: 13px;'> $int_location </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class='row'>
+                      <div class='col col-lg-12'>
+                        <div class='shadow-sm p-3 mb-5 bg-white rounded'>$crdate
+                            <div>
+                                <!-- fot the ext bod -->
+                                <p><b>Dear Director/b></p>
+                                <p>We wish to inform you that a <b>Vault - In</b> transaction recently occurred in your Vault.
+                                Please find below details of the transaction:</p>
+                            </div>
+                            <p>
+                                <div class='shadow p-3 mb-5 bg-white rounded'>Transaction Details - <b>Vault - In</b></div>
+                                <table class='table table-borderless'>
+                                    <tbody>
+                                        <div>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transfer From</b></td>
+                                        <td style='font-size: 12px;'>$tellname</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Description</b></td>
+                                        <td style='font-size: 12px;'>$description</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Reference Id</b></td>
+                                        <td style='font-size: 12px;'>$transact_id</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transaction Amount</b></td>
+                                        <td style='font-size: 12px;'>$amount</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transaction Date/Time</b></td>
+                                        <td style='font-size: 12px;'>$transdate</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Account Balance</b></td>
+                                        <td style='font-size: 12px;'>&#8358; $new_vaultbalance</td>
+                                      </tr>
+                                    </tbody>
+                                </div>
+                                  </table>
+                            </p>
+                            <button type='button' class='btn btn-primary btn-lg btn-block'> <b style='font-size: 15px;'>Print Account Statement</b></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <!-- Optional JavaScript -->
+                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                <script src='https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossorigin='anonymous'></script>
+                <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>
+                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin='anonymous'></script>
+              </body>
+            </html>";
+            $mail->AltBody = "This is the plain text version of the email content";
+            // mail system
+            if(!$mail->send()) 
+               {
+                $_SESSION["Lack_of_intfund_$randms"] = "";
+                echo "error";
+                echo header ("Location: ../mfi/teller_journal.php?message1=$randms");
+               } else
+               {
+                $_SESSION["Lack_of_intfund_$randms"] = "";
+                echo "error";
+                echo header ("Location: ../mfi/teller_journal.php?message1=$randms");
+               }
                     }
                     else{
                         $_SESSION["Lack_of_intfund_$randms"] = "";
@@ -93,9 +201,107 @@ if (isset($_POST['transact_id']) && isset($_POST['type'])) {
                         '{$tid}', '{$amount}')";
                         $rin = mysqli_query($connection, $record);
                     if($rin){
-                        $_SESSION["Lack_of_intfund_$randms"] = "Registration Failed";
-                        echo "error";
-                        echo header ("Location: ../mfi/teller_journal.php?message3=$randms");
+                        $mail = new PHPMailer;
+            $mail->From = $int_email;
+            $mail->FromName = $int_name;
+            $mail->addAddress('hesanmal316@gmail.com');
+            $mail->addReplyTo($int_email, "Reply");
+            $mail->isHTML(true);
+            $mail->Subject = "Transaction Alert from $int_name";
+            $mail->Body = "<!doctype html>
+            <html lang='en'>
+              <head>
+                <!-- Required meta tags -->
+                <meta charset='utf-8'>
+                <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+                <!-- Bootstrap CSS -->
+                <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh' crossorigin='anonymous'>
+            
+                <title>Transaction Alert</title>
+              </head>
+              <body>
+                <div class='container'>
+                    <div class='row justify-content-md-center'>
+                      <div class='col col-lg-6'>
+                        <div class='shadow p-3 mb-5 bg-white rounded'>
+                            <!-- int logo -->
+                            <div class='row justify-content-md-center'>
+                                <img src='$int_logo' height='60px' width='60px' alt='int image' class='rounded mx-auto d-block'>
+                                <div class='spinner-grow text-primary' role='status'>
+                                    <span class='sr-only'>Loading...</span>
+                                  </div>
+                            </div>
+                            <span> <b>$int_name</b> </span> || <span class='lead' style='font-size: 13px;'> $int_location </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div class='row'>
+                      <div class='col col-lg-12'>
+                        <div class='shadow-sm p-3 mb-5 bg-white rounded'>$crdate
+                            <div>
+                                <!-- fot the ext bod -->
+                                <p><b>Dear Director/b></p>
+                                <p>We wish to inform you that a <b>Vault - In</b> transaction recently occurred in your Vault.
+                                Please find below details of the transaction:</p>
+                            </div>
+                            <p>
+                                <div class='shadow p-3 mb-5 bg-white rounded'>Transaction Details - <b>Vault - In</b></div>
+                                <table class='table table-borderless'>
+                                    <tbody>
+                                        <div>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transfer From</b></td>
+                                        <td style='font-size: 12px;'>$tellname</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Description</b></td>
+                                        <td style='font-size: 12px;'>$description</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Reference Id</b></td>
+                                        <td style='font-size: 12px;'>$transact_id</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transaction Amount</b></td>
+                                        <td style='font-size: 12px;'>$amount</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Transaction Date/Time</b></td>
+                                        <td style='font-size: 12px;'>$transdate</td>
+                                      </tr>
+                                      <tr>
+                                        <td style='font-size: 12px;'> <b>Account Balance</b></td>
+                                        <td style='font-size: 12px;'>&#8358; $new_vaultbalance</td>
+                                      </tr>
+                                    </tbody>
+                                </div>
+                                  </table>
+                            </p>
+                            <button type='button' class='btn btn-primary btn-lg btn-block'> <b style='font-size: 15px;'>Print Account Statement</b></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                <!-- Optional JavaScript -->
+                <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                <script src='https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossorigin='anonymous'></script>
+                <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossorigin='anonymous'></script>
+                <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossorigin='anonymous'></script>
+              </body>
+            </html>";
+            $mail->AltBody = "This is the plain text version of the email content";
+            // mail system
+            if(!$mail->send()) 
+               {
+                $_SESSION["Lack_of_intfund_$randms"] = "Registration Failed";
+                echo "error";
+                echo header ("Location: ../mfi/teller_journal.php?message3=$randms");
+               } else
+               {
+                $_SESSION["Lack_of_intfund_$randms"] = "Registration Failed";
+                echo "error";
+                echo header ("Location: ../mfi/teller_journal.php?message3=$randms");
+               }
                     }
                     else{
                         $_SESSION["Lack_of_intfund_$randms"] = "";
---
+++ mfi/vault_report.php
@@ -0,0 +1,103 @@
+<?php
+
+$page_title = "Vault Report";
+$destination = "index.php";
+    include("header.php");
+?>
+
+<?php
+function fill_client($connection) {
+  $sint_id = $_SESSION["int_id"];
+  $org = "SELECT * FROM branch WHERE int_id = '$sint_id'";
+  $res = mysqli_query($connection, $org);
+  $out = '';
+  while ($row = mysqli_fetch_array($res))
+  {
+    $out .= '<option value="'.$row["id"].'">'.$row["name"].'</option>';
+  }
+  return $out;
+}
+?>
+<!-- Content added here -->
+    <div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Vault Report</h4>
+                  <!-- <p class="card-category">Fill in all important data</p> -->
+                </div>
+                <div class="card-body">
+                  <form>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Start Date</label>
+                          <input type="date" value="" name="" class="form-control" id="start">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">End Date</label>
+                          <input type="date" value="" name="" class="form-control" id="end">
+                          <input type="text" id="int_id" hidden name="" value="<?php echo $sessint_id;?>" class="form-control" readonly>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                        <label class="bmd-label-floating">Branch</label>
+                          <select name="branch" class="form-control" id="input" required>
+                          <option value="">select an option</option>
+                          <?php echo fill_client($connection); ?>
+                          </select>
+                          
+                        </div>
+                      </div>
+                      
+                      <div class="clearfix"></div>
+                    </div>
+                  </form>
+                  <button type="reset" class="btn btn-danger pull-left">Reset</button>
+                  <button id="runi" class="btn btn-primary pull-right">Run Report</button>
+                      </div>
+                      <!-- <button type="reset" class="btn btn-danger pull-left">Reset</button> -->
+                    <!-- <button class="btn btn-primary pull-right">Run Report</button> -->
+                  <!-- writing a code to the run the reort at click -->
+                  <script>
+                    $(document).ready(function () {
+                      $('#runi').on("change keyup paste click", function () {
+                        var start = $('#start').val();
+                        var end = $('#end').val();
+                        var branch = $('#input').val();
+                        $.ajax({
+                          url: "ajax_post/run_vault_report.php", 
+                          method: "POST",
+                          data:{start:start, end:end, branch:branch},
+                          success: function (data) {
+                            $('#outjournal').html(data);
+                          }
+                        })
+                      });
+                    });
+                  </script>
+                  <!-- this section is the end of run report -->
+                  
+                </div>
+              </div>
+            </div>
+            <!-- teller report -->
+            <!-- populate for print with above data --> 
+            <div id="outjournal"></div>
+            <!-- end  -->
+          </div>
+          <!-- /content -->
+        </div>
+      </div>
+
+<?php
+
+    include("footer.php");
+
+?>
