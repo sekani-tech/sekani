413f04a0909cf0bf7ed3a4a49c26498871f39b46
flash code <47109193+flash-code1@users.noreply.github.com>
2020-06-16 08:11:45 +0100

    pushing up

--- mfi/date_calculation.php
+++ mfi/date_calculation.php
@@ -1,27 +1,28 @@
 <?php
-$loan_term = $_POST["loant"] - 1;
+$loan_term = $_POST["loant"];
 $repayment_start = $_POST["repay_start"];
 $rep_every = $_POST["repay"];
 $disburse_date = $_POST["disbd"];
 $i = 0;
 $table_day = "";
 $add_date = "";
+$loan_term1x = $loan_term - 1;
 if($rep_every == "month"){
     $table_day = "Months";
     // $add_date = "30";
-    $actualend_date = date('Y-m-d', strtotime("+".$loan_term." months", strtotime($repayment_start)));
+    $actualend_date = date('Y-m-d', strtotime("+".$loan_term1x." months", strtotime($repayment_start)));
 }else if($rep_every == "day"){
     $table_day = "Days";
     // $add_date = "1";
-    $actualend_date = date('Y-m-d', strtotime("+".$loan_term." days", strtotime($repayment_start)));
+    $actualend_date = date('Y-m-d', strtotime("+".$loan_term1x." days", strtotime($repayment_start)));
 }else if($rep_every == "year"){
     $table_day = "Years";
     $add_date = "365";
-    $actualend_date = date('Y-m-d', strtotime("+".$loan_term." years", strtotime($repayment_start)));
+    $actualend_date = date('Y-m-d', strtotime("+".$loan_term1x." years", strtotime($repayment_start)));
 } else if($rep_every == "week"){
     $table_day = "Weeks";
     // $add_date = "7";
-    $actualend_date = date('Y-m-d', strtotime("+".$loan_term." weeks", strtotime($repayment_start)));
+    $actualend_date = date('Y-m-d', strtotime("+".$loan_term1x." weeks", strtotime($repayment_start)));
 }
 else{
     $table_day = "days";
