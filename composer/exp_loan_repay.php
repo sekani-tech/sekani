742bacf9b61c3bd9c8d94486ea6eb10d919e9fe8
H3SAN <46991842+H3SAN@users.noreply.github.com>
2020-06-24 08:42:07 +0100

    lost count

---
+++ composer/exp_loan_repay.php
@@ -0,0 +1,108 @@
+<?php
+$output = '';
+include("../functions/connect.php");
+session_start();
+?>
+<?php
+  $intname = $_SESSION['int_name'];
+  $branch_id = $_SESSION["branch_id"];
+  $date = date('d/m/Y');
+  // $staff = $_POST["staff"];
+  $branchquery = mysqli_query($connection, "SELECT * FROM branch WHERE id='$branch_id'");
+    if (count([$branchquery]) == 1) {
+      $ans = mysqli_fetch_array($branchquery);
+      $branch = $ans['name'];
+      $branch_email = $ans['email'];
+      $branch_location = $ans['location'];
+      $branch_phone = $ans['phone'];
+    }
+    $current = date('Y-m-d');
+  function fill_report($connection)
+        {
+            $out = '';
+            $sessint_id = $_SESSION['int_id'];
+          // import
+        //   $glcode = $_POST['glcode'];
+        $currentdate = date('Y-m-d');
+        $query = "SELECT * FROM loan WHERE int_id = '$sessint_id' AND repayment_date = '$currentdate'";
+        $result = mysqli_query($connection, $query);
+        while ($q = mysqli_fetch_array($result, MYSQLI_ASSOC))
+          {
+            $name = $q['client_id'];
+            $anam = mysqli_query($connection, "SELECT firstname, lastname FROM client WHERE id = '$name'");
+            $f = mysqli_fetch_array($anam);
+            $nae = strtoupper($f["firstname"]." ".$f["lastname"]);
+            $principal = number_format($q["principal_amount"]);
+            $loant = $q["loan_term"];
+            $disb_date = $q["disbursement_date"];
+            $repay = $q["repayment_date"];
+            $loant = $q["loan_term"];
+            $bal = $q["total_outstanding_derived"];
+
+            $out .= '
+            <tr>
+            <th style="font-size: 50px;" class="column1">'.$nae.'</th>
+            <th style="font-size: 50px;" class="column1">'.$principal.'</th>
+            <th style="font-size: 50px;" class="column1">'.$loant.'</th>
+            <th style="font-size: 50px;" class="column1">'.$disb_date.'</th>
+            <th style="font-size: 50px;" class="column1">'.$repay.'</th>
+            <th style="font-size: 50px;" class="column1">'.$bal.'</th>
+            </tr>
+          ';
+          }
+        // }
+          return $out;
+        }
+  require_once __DIR__ . '/vendor/autoload.php';
+  $mpdf = new \Mpdf\Mpdf();
+  $mpdf->SetWatermarkImage(''.$_SESSION["int_logo"].'');
+  $mpdf->showWatermarkImage = true;
+  $mpdf->WriteHTML('<link rel="stylesheet" media="print" href="pdf/style.css" media="all"/>
+  <header class="clearfix">
+<div id="logo">
+  <img src="'.$_SESSION["int_logo"].'" height="80" width="80">
+</div>
+<h1>'.$_SESSION["int_full"].' <br/>Loans to be paid on '.$current.'</h1>
+<div id="company" class="clearfix">
+  <div>'.$branch.'</div>
+  <div>'.$branch_location.'</div>
+  <div>(+234) '.$branch_phone.'</div>
+  <div><a href="mailto:'.$branch_email.'">'.$branch_email.'</a></div>
+</div>
+<div id="project">
+  <div><span>BRANCH</span> '.$branch.' </div>
+</div>
+</header>
+  <main>
+  <table>
+  <thead class=" text-primary">
+  <tr>
+      <th style="font-size: 50px;" class="column1">
+      Client Name
+    </th>
+    <th style="font-size: 50px;" class="column1">
+    Principal Amount
+    </th>
+    <th style="font-size: 50px;" class="column1">
+    Loan Term
+    </th>
+    <th style="font-size: 50px;" class="column1">
+    Disbursement Date
+    </th>
+    <th style="font-size: 50px;" class="column1">
+   Repayment Date
+    </th>
+    <th style="font-size: 50px;" class="column1">
+    Outstanding Loan Balance
+    </th>
+      </tr>
+    </thead>
+  <tbody>
+  "'.fill_report($connection).'"
+  </tbody>
+  </table>
+  </main>
+  ');
+  $file_name = 'Expected loan Repayments for '.$intname.'-'.$date.'.pdf';
+  $mpdf->Output($file_name, 'D');
+?>
---
+++ mfi/bank_transfer.php
@@ -0,0 +1,135 @@
+<?php
+
+$page_title = "Cash Transaction";
+$destination = "transaction.php";
+    include("header.php");
+?>
+<?php
+// right now we will program
+// first step - check if this person is authorized
+// if ($valut == 1 || $valut == "1") {
+?>
+<?php
+// output the branch name
+  $bch_name =  mysqli_query($connection, "SELECT * FROM branch WHERE id = '$bch_id' && int_id = '$sessint_id'");
+  $grn = mysqli_fetch_array($bch_name);
+  $get_b_r_n = $grn['id'];
+  $bname = $grn['name'];
+// Ending branch_name
+
+// check the current balance
+$int_balance = mysqli_query($connection, "SELECT * FROM int_vault WHERE branch_id = '$bch_id' && int_id = '$sessint_id'");
+$itb = mysqli_fetch_array($int_balance);
+$g_i_t_b = $itb['balance'];
+$vault_limit = $itb['movable_amount'];
+$vault_last_with = $itb['last_withdrawal'];
+$vault_last_dep = $itb['last_deposit'];
+
+// For the Transaction ID auto generated
+$digits = 10;
+$transaction_id = str_pad(rand(0, pow(10, 7)-1), 7, '0', STR_PAD_LEFT);
+// check if every data is active
+// then we will do the transaction - stored 
+// then if will reflect inside of int_transaction for the teller that will be picked
+// then you can display a message that the transaction is successful.
+// remember to add code of making institution branch add a vualt and also Super Admin
+?>
+<div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Cash Transfer</h4>
+                  <!-- <p class="card-category">Fill in all important data</p> -->
+                </div>
+                <div class="card-body">
+                  <form action="#" method="POST">
+                    <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Branch Name</label>
+                          <!-- populate available balance -->
+                          <input type="text" value="<?php echo $bname; ?>" name="dd" id="branch_id" class="form-control"  readonly>
+                          <input type="text" value="<?php echo $get_b_r_n; ?>" name="branch" hidden id="branch_id" class="form-control" readonly>
+                        </div>
+                      </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <!-- populate from db -->
+                          <label class="bmd-label-floating">Transfer From:</label>
+                          <select name="type" id="selctttype" class="form-control">
+                            <option value="0">select a transaction type in/out</option>
+                            <option value="vault_in">DEPOSIT INTO VAULT</option>
+                            <option value="vault_out">WITHDRAW FROM VAULT</option>
+                            <option value="from_bank">FROM BANK TO VAULT</option>
+                            <option value="to_bank">FROM VAULT TO BANK</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Amount</label>
+                          <input  type="number" name="amount" id="" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Transaction id</label>
+                          <!-- populate available balance -->
+                          <input type="text" value="<?php echo $transaction_id; ?>" name="transact_id" id="" class="form-control" readonly>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Transfer to:</label>
+                          <!-- populate available balance -->
+                          <input type="text" value="" name="balance" id="" class="form-control" >
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Amount</label>
+                          <!-- populate available balance -->
+                          <input type="text" value="" name="balance" id="" class="form-control" >
+                        </div>
+                      </div>
+                      </div>
+                      <button type="reset" class="btn btn-danger pull-left">Reset</button>
+                    <button type="submit" class="btn btn-primary pull-right">submit</button>
+                    <div class="clearfix"></div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /content -->
+        </div>
+      </div>
+      <?php
+    include("footer.php");
+?>
+<?php
+// } else {
+//   echo '<script type="text/javascript">
+//   $(document).ready(function(){
+//    swal({
+//     type: "error",
+//     title: "Vault Authorization",
+//     text: "You Dont Have permission to Make Transaction From Vault",
+//    showConfirmButton: false,
+//     timer: 2000
+//     }).then(
+//     function (result) {
+//       history.go(-1);
+//     }
+//     )
+//     });
+//    </script>
+//   ';
+//   // $URL="transact.php";
+//   // echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+// }
+
+?>
