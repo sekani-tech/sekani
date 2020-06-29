82461f4e80b7d9d8eebf052d45228f562f1a8609
H3SAN <46991842+H3SAN@users.noreply.github.com>
2020-06-29 13:57:44 +0100

    fvdfvfvdfv

--- mfi/items/pending_group.php
+++ mfi/items/pending_group.php
@@ -1,30 +1,47 @@
 <script>
                   $(document).ready(function() {
-                  $('#tabledat').DataTable();
+                  $('#tabledats').DataTable();
                   });
                   </script>
                   
                    <div class="table-responsive">
-                    <table id="tabledat" class="table" cellspacing="0" style="width:100%">
+                   <table id="tabledats" class="table" cellspacing="0" style="width:100%">
                       <thead class=" text-primary">
-                      
+                      <?php
+                        $query = "SELECT * FROM groups WHERE int_id = '$sessint_id' && status = 'Pending'";
+                        $result = mysqli_query($connection, $query);
+                      ?>
                         <th>
                           Group
                         </th>
                         <th>
                           Branch
                         </th>
-                        <!-- <th>View</th> -->
-                        <th> </th>
+                        <th width="20px">Edit</th>
                         <!-- <th>Phone</th> -->
                       </thead>
                       <tbody>
+                      <?php if (mysqli_num_rows($result) > 0) {
+                        while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {?>
                         <tr>
-                          <th></th>
-                          <th></th>
-                          <!-- <td><a href="update_group.php?edit=" class="btn btn-info">Edit</a></td> -->
-                          <td><a href="close_group.php?edit=" class="btn btn-info">Approve</a></td>
+                        <?php $row["id"]; ?>
+                          <th><?php echo $row["g_name"]; ?></th>
+                          <?php
+                          $ds= $row["branch_id"];
+                           $query = "SELECT * FROM branch WHERE int_id = '$sessint_id' && id = '$ds'";
+                           $erre = mysqli_query($connection, $query);
+                           $ds = mysqli_fetch_array($erre);
+                           $dfd = $ds['name'];
+                          ?>
+                          <th><?php echo $dfd; ?></th>
+                          <td><a href="update_group.php?edit=<?php echo $row["id"];?>" class="btn btn-info">View</a></td>
                         </tr>
+                        <?php }
+                          }
+                          else {
+                            // echo "0 Document";
+                          }
+                          ?>
                       </tbody>
                     </table>
                   </div>
