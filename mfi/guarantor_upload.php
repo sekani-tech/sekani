f03a451b72052b0833ce6f3dd8fe98010696b30c
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-07-09 14:44:38 +0100

    pushing it up

--- mfi/guarantor_upload.php
+++ mfi/guarantor_upload.php
@@ -18,24 +18,41 @@
       $int_id = $a['int_id'];
      }
 
-    $coll = "INSERT INTO loan_gaurantor (int_id, client_id, first_name, last_name, phone, phone2, home_address, office_address, email) VALUES ( '{$int_id}','{$client_id}','{$firstname}','{$lastname}','{$phone}','{$phone_b}','{$h_address}',
-    '{$o_address}','{$email}')";
-
-    $query = mysqli_query($connection, $coll);
-
-    if($query){
-        $don = "SELECT * FROM loan_gaurantor WHERE client_id = '$client_id'";
-        $result = mysqli_query($connection, $don);
-        while ($row = mysqli_fetch_array($result)) {
-            $display = '
-            <tr>
-            <td>'.$row["first_name"].'</td>
-            <td>'.$row["phone"].'</td>
-            <td>'.$row["email"].'</td>
-            </tr>
-            ';
-            echo $display;
+    $get_gau = mysqli_query($connection, "SELECT * FROM loan_gaurantor WHERE client_id = '$client_id' AND phone = '$phone' OR phone2 = '$phone_b' OR email = '$h_address'");
+    $mp = mysqli_num_rows($get_gau);
+    if ($mp <= 0) {
+        $coll = "INSERT INTO loan_gaurantor (int_id, client_id, first_name, last_name, phone, phone2, home_address, office_address, email) VALUES ( '{$int_id}','{$client_id}','{$firstname}','{$lastname}','{$phone}','{$phone_b}','{$h_address}',
+        '{$o_address}','{$email}')";
+    
+        $query = mysqli_query($connection, $coll);
+    
+        if($query){
+            $don = "SELECT * FROM loan_gaurantor WHERE client_id = '$client_id'";
+            $result = mysqli_query($connection, $don);
+            while ($row = mysqli_fetch_array($result)) {
+                $display = '
+                <tr>
+                <td>'.$row["first_name"].'</td>
+                <td>'.$row["phone"].'</td>
+                <td>'.$row["email"].'</td>
+                </tr>
+                ';
+                echo $display;
+            }
+            
         }
-        
+    } else {
+        echo '<script type="text/javascript">
+        $(document).ready(function(){
+            swal({
+                type: "error",
+                title: "Gaurantor Exist",
+                text: "Your cant have more than one gaurantor",
+                showConfirmButton: false,
+                timer: 3000
+            });
+        });
+        </script>
+        ';
     }
 ?>
