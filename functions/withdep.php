fd19bef56db051384c0948a026194766d3b45f8c
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-07-18 13:16:23 +0100

    making a good push/for the new code instance

--- functions/withdep.php
+++ functions/withdep.php
@@ -193,7 +193,42 @@ if ($is_del == "0" && $is_del != NULL) {
             $update_arrear = mysqli_query($connection, "UPDATE `loan_arrear` SET principal_amount = '0.00', interest_amount = '0.00', installment = '0' WHERE id = '$a_id' AND int_id = '$a_int_id' AND client_id = '$client_id'");
             // check out the update
             if ($update_arrear) {
+            //   $loan_bal = $amt / 2;
+            // $loan_bal_prin = $a_principal;
+            // $loan_bal_int = $a_interest;
               // OK NOW RUN A FUNCTION.
+              $updated_loan_port = $newbalport + $a_principal;
+              $intloan_port = $intbalport + $a_interest;
+              $collection_principal = $a_principal;
+              $collection_interest = $a_interest;
+              $update_the_loan = mysqli_query($connection, "UPDATE `acc_gl_account` SET organization_running_balance_derived = '$updated_loan_port' WHERE int_id ='$sessint_id' AND gl_code = '$loan_port'");
+              // Qwerty
+              if ($update_the_loan) {
+                // damn with
+                $insert_loan_port = mysqli_query($connection, "INSERT INTO `gl_account_transaction` (`int_id`, `branch_id`, `gl_code`, `transaction_id`, `description`, `transaction_type`, `teller_id`, `is_reversed`, `transaction_date`,
+                 `amount`, `gl_account_balance_derived`, `overdraft_amount_derived`, `balance_end_date_derived`, `balance_number_of_days_derived`, `cumulative_balance_derived`, `created_date`, `manually_adjusted_or_reversed`, `credit`, `debit`) 
+                VALUES ('{$int_id}', '{$branch_id}', '{$loan_port}', '{$transid}', 'Loan Repayment Principal / {$clientt_name}', 'Loan Repayment Principal', '0', '0', '{$gen_date}',
+                 '{$collection_principal}', '{$updated_loan_port}', '{$updated_loan_port}', '{$gen_date}', '0', '0', '{$gen_date}', '0', '{$collection_principal}', '0.00')");
+                 if ($insert_loan_port) {
+                  //  go for the interest
+                  $update_the_int_loan = mysqli_query($connection, "UPDATE acc_gl_account SET organization_running_balance_derived = '$intloan_port' WHERE int_id = '$sessint_id' AND gl_code ='$int_loan_port'");
+                  if ($update_the_int_loan) {
+                    $insert_i_port = mysqli_query($connection, "INSERT INTO `gl_account_transaction` (`int_id`, `branch_id`, `gl_code`, `transaction_id`, `description`, `transaction_type`, `teller_id`, `is_reversed`, `transaction_date`,
+             `amount`, `gl_account_balance_derived`, `overdraft_amount_derived`, `balance_end_date_derived`, `balance_number_of_days_derived`, `cumulative_balance_derived`, `created_date`, `manually_adjusted_or_reversed`, `credit`, `debit`) 
+            VALUES ('{$int_id}', '{$branch_id}', '{$int_loan_port}', '{$transid}', 'Loan Repayment Interest / {$clientt_name}', 'Loan Repayment Interest', '0', '0', '{$gen_date}',
+             '{$collection_interest}', '{$intloan_port}', '{$intloan_port}', '{$gen_date}', '0', '0', '{$gen_date}', '0', '{$collection_interest}', '0.00')");
+                    // done
+                  } else {
+                    echo "LOAN INTEREST BAD";
+                  }
+                 } else {
+                   echo "LOAN PRIN. INTEREST BAD";
+                 }
+                } else 
+                {
+                  echo "GL UPDATE BAD";
+                }
+                // sec wise
             }
           } else if ($amt < $loan_amount) {
             // ok nice
@@ -204,6 +239,38 @@ if ($is_del == "0" && $is_del != NULL) {
             $update_arrear = mysqli_query($connection, "UPDATE `loan_arrear` SET principal_amount = '$loan_bal_prin', interest_amount = '$loan_bal_int', installment = '0' WHERE id = '$a_id' AND int_id = '$a_int_id' AND client_id = '$client_id'");
             if ($update_arrear) {
               // OK NOW RUN A FUNCTION.
+              $updated_loan_port = $newbalport + $loan_bal;
+              $intloan_port = $intbalport + $loan_bal; 
+              $collection_principal = $loan_bal;
+              $collection_interest = $loan_bal;
+              $update_the_loan = mysqli_query($connection, "UPDATE `acc_gl_account` SET organization_running_balance_derived = '$updated_loan_port' WHERE int_id ='$sessint_id' AND gl_code = '$loan_port'");
+              // Qwerty
+              if ($update_the_loan) {
+                // damn with
+                $insert_loan_port = mysqli_query($connection, "INSERT INTO `gl_account_transaction` (`int_id`, `branch_id`, `gl_code`, `transaction_id`, `description`, `transaction_type`, `teller_id`, `is_reversed`, `transaction_date`,
+                 `amount`, `gl_account_balance_derived`, `overdraft_amount_derived`, `balance_end_date_derived`, `balance_number_of_days_derived`, `cumulative_balance_derived`, `created_date`, `manually_adjusted_or_reversed`, `credit`, `debit`) 
+                VALUES ('{$int_id}', '{$branch_id}', '{$loan_port}', '{$transid}', 'Loan Repayment Principal / {$clientt_name}', 'Loan Repayment Principal', '0', '0', '{$gen_date}',
+                 '{$collection_principal}', '{$updated_loan_port}', '{$updated_loan_port}', '{$gen_date}', '0', '0', '{$gen_date}', '0', '{$collection_principal}', '0.00')");
+                 if ($insert_loan_port) {
+                  //  go for the interest
+                  $update_the_int_loan = mysqli_query($connection, "UPDATE acc_gl_account SET organization_running_balance_derived = '$intloan_port' WHERE int_id = '$sessint_id' AND gl_code ='$int_loan_port'");
+                  if ($update_the_int_loan) {
+                    $insert_i_port = mysqli_query($connection, "INSERT INTO `gl_account_transaction` (`int_id`, `branch_id`, `gl_code`, `transaction_id`, `description`, `transaction_type`, `teller_id`, `is_reversed`, `transaction_date`,
+             `amount`, `gl_account_balance_derived`, `overdraft_amount_derived`, `balance_end_date_derived`, `balance_number_of_days_derived`, `cumulative_balance_derived`, `created_date`, `manually_adjusted_or_reversed`, `credit`, `debit`) 
+            VALUES ('{$int_id}', '{$branch_id}', '{$int_loan_port}', '{$transid}', 'Loan Repayment Interest / {$clientt_name}', 'Loan Repayment Interest', '0', '0', '{$gen_date}',
+             '{$collection_interest}', '{$intloan_port}', '{$intloan_port}', '{$gen_date}', '0', '0', '{$gen_date}', '0', '{$collection_interest}', '0.00')");
+                    // done
+                  } else {
+                    echo "LOAN INTEREST BAD";
+                  }
+                 } else {
+                   echo "LOAN PRIN. INTEREST BAD";
+                 }
+                } else 
+                {
+                  echo "GL UPDATE BAD";
+                }
+                // sec wise
             }
           }
           // MAKING IT OUT
