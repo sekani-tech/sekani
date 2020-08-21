e5e638e3c1e9dd9f9c6890040b8cb47ef72ce192
H3SAN <46991842+H3SAN@users.noreply.github.com>
2020-08-21 13:21:51 +0100

    Pushing to admin

--- mfi/ajax_post/sub_ajax/teller_balance.php
+++ mfi/ajax_post/sub_ajax/teller_balance.php
@@ -21,4 +21,21 @@ session_start();
   ';
     echo $output;
   }
+  else if (isset($_POST['ib'])) {
+    $name = $_POST['ib'];
+    $int_id = $_SESSION['int_id'];
+    $branchid = $_SESSION['branch_id'];
+  
+    $que = "SELECT * FROM acc_gl_account WHERE gl_code = '$name' AND int_id = '$int_id' AND branch_id = '$branchid'";
+    $roc = mysqli_query($connection, $que);
+    $yn = mysqli_fetch_array($roc);
+    $balance = $yn['organization_running_balance_derived'];
+
+    $output = '<div class="form-group">
+    <label>Account Balance:</label>
+    <input type="text" value="'.number_format($balance, 2).'" name="principal_amoun" class="form-control" readonly required id="principal_amount">
+  </div>
+  ';
+    echo $output;
+  }
     ?>
