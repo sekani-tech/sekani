7f0c4ab2d9b59c9bc592ca2fdd5bc0e6f1b88936
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-08-28 14:05:21 +0100

    DATABASE ON ADMIN

--- mfi/header.php
+++ mfi/header.php
@@ -154,7 +154,7 @@ setInterval(function() {
   <script src="../datatable/jquery-3.3.1.min.js"></script>
   <script src="../datatable/jquery-3.3.2.js"></script>
   <script src="../datatable/jquery.dataTables.min.js"></script>
-  <script src="../datatable/dataTables.bootstrap.min.js"></script>
+  <!-- <script src="../datatable/dataTables.bootstrap.min.js"></script> -->
   <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.1.3/materia/bootstrap.min.css"> -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <style>
