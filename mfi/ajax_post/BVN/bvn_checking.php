07fedda0cafd840c9147a34e0a1bd9c64a96e9bb
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-09-07 08:29:59 +0100

    make a push

--- mfi/ajax_post/BVN/bvn_checking.php
+++ mfi/ajax_post/BVN/bvn_checking.php
@@ -140,6 +140,7 @@ if ($bvn_length == 11) {
             ';
             ?>
             <!-- popup -->
+            <!-- QWERTY -->
             <div class="modal fade bd-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
               <div class="modal-dialog modal-lg">
                 <div class="modal-content">
@@ -329,8 +330,21 @@ if ($bvn_length == 11) {
             </div>
             <!-- profile picture -->
             <div class="col-md-5 mr-auto">
-                <img src="data:image/png;base64,<?php echo $bvn_image; ?>" alt="" srcset="" height="400px" width="400px">
+              <div class="image-box">
+              <img src="data:image/png;base64,<?php echo $bvn_image; ?>" alt="" srcset="" height="400px" width="400px">
+              </div>
             </div>
+            <style>
+            img {
+max-width: 100%;
+max-height: 100%;
+object-fit: contain;
+}
+.image-box {
+height: 444px;
+width: 770px;
+}
+        </style>
           </div>
                 </div>
               </div>
