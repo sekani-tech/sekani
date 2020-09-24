7f9210bffec67500b247bc0136f4c3fb6419ae02
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-09-24 09:34:22 +0100

    MAKING A PUSH

---
+++ mfi/loan_reconciliation.php
@@ -0,0 +1,47 @@
+<?php
+$page_title = "Loan Reconciliation";
+$destination = "configuration.php";
+include("header.php");
+?>
+<!-- do your front end -->
+<div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title ">Loan Reconciliation</h4>
+              
+                  <!-- Insert number users institutions -->
+                </div>
+                <div class="card-body">
+                  <div class="table-responsive">
+                  <table class="rtable display nowrap" style="width:100%">
+                      <thead class=" text-primary">
+                        <tr>
+                          <th></th>
+                          <th></th>
+                          <th></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                      <tr>
+                          <th>Details</th>
+                          <th>Setup the Depreciation Value for the Assets in the institution</th>
+                          <td><a href="#" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
+                        </tr>
+                        <tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+<!-- end your front end -->
+<?php
+include("footer.php");
+?>
