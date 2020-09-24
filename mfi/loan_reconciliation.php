f78a6dca9d0731cedbbea67a0eac3320b86dbd1d
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2020-09-24 11:20:26 +0100

    loan reconciliation

--- mfi/loan_reconciliation.php
+++ mfi/loan_reconciliation.php
@@ -20,16 +20,35 @@ include("header.php");
                   <table class="rtable display nowrap" style="width:100%">
                       <thead class=" text-primary">
                         <tr>
-                          <th></th>
-                          <th></th>
-                          <th></th>
+                          <th>Account Name</th>
+                          <th>Account Number</th>
+                          <th>Loan Product</th>
+                          <th>Interest Rate</th>
+                          <th>Principal Amount</th>
+                          <th>Loan Outstanding</th>
+                          <th>Edit</th>
                         </tr>
                       </thead>
                       <tbody>
-                      <tr>
-                          <th>Details</th>
-                          <th>Setup the Depreciation Value for the Assets in the institution</th>
-                          <td><a href="#" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
+                        <tr>
+                          <td>Dennis</td>
+                          <td>1023465798</td>
+                          <td>120,000</td>
+                          <td>10</td>
+                          <td>2,000,000</td>
+                          <td>3,000,000</td> 
+                          <td>
+                          <div class="btn-group">
+                            <button type="button" class="btn btn-success">View</button>
+                            <button type="button" class="btn btn-secondary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span class="sr-only">Toggle Dropdown</span>
+                            </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Pay Loan</a>
+    <a class="dropdown-item" href="#">Edit Loan Repayment</a>
+  </div>
+</div> 
+                          </td>                         
                         </tr>
                         <tr>
                       </tbody>
