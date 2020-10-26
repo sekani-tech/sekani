95f7788e77de3140390602ba907056e3839ed34e
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-26 11:29:29 +0100

    CHCKING

--- script/REMODEL/restructure_loan_interest.php
+++ script/REMODEL/restructure_loan_interest.php
@@ -1,7 +1,7 @@
 <?php
 include("../../functions/connect.php");
 // START -- REALAID
-$query_the_realaid = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE status = '0'");
+$query_the_realaid = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE `status` = '0'");
 
 if (mysqli_num_rows($query_the_realaid) > 0) {
     while($q = mysqli_fetch_array($query_the_realaid)) {
@@ -11,29 +11,30 @@ if (mysqli_num_rows($query_the_realaid) > 0) {
         $interest_rate = $q["interest_rate"];
         $repayment_date = $q["repayment_date"];
         // realaid_loan_interest_remodel SHOUT
-        $query_makeitup = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE interest_rate IS NULL AND status = '0'");
+        $query_makeitup = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE interest_rate IS NULL AND `status` = '0'");
         if (mysqli_num_rows($query_makeitup) > 0) {
             // realaid
             while ($yu = mysqli_fetch_array($query_makeitup)) {
                 $d_c_id = $yu["client_id"];
                 $query_delete_loanremodel = mysqli_query($connection, "DELETE FROM `loan_remodeling_reform` WHERE client_id = '$d_c_id'");
                 if ($query_delete_loanremodel) {
-                    echo "DONE WITH DELETE";
-                    $query_update_realaid = mysqli_query($connection, "UPDATE `realaid_loan_interest_remodel` SET status = '3' WHERE id = '$qid'");
+                    echo "...";
+                    $query_update_realaid = mysqli_query($connection, "UPDATE `realaid_loan_interest_remodel` SET `status` = '3' WHERE id = '$qid'");
                 } else {
                     echo "ERROR ON DELETE";
                 }
             }
         } else {
-            $query_the_repaymenttable = mysqli_query($connection, "UPDATE `loan_remodeling_reform` SET interest_rate = '$interest_rate', repayment_date = '$repayment_date', status = '1' WHERE int_id = '13' AND client_id = '$client_id'");
+            echo "non";
+        }
+        // end
+        $query_the_repaymenttable = mysqli_query($connection, "UPDATE `loan_remodeling_reform` SET interest_rate = '$interest_rate', repayment_date = '$repayment_date', status = '1' WHERE int_id = '13' AND client_id = '$client_id'");
                         if ($query_the_repaymenttable) {
                           echo "Good";
-                          $query_update_realaid = mysqli_query($connection, "UPDATE `realaid_loan_interest_remodel` SET status = '1' WHERE id = '$qid'");
+                          $query_update_realaid = mysqli_query($connection, "UPDATE `realaid_loan_interest_remodel` SET `status` = '1' WHERE id = '$qid'");
                         } else {
                           echo "Bad";
                         }
-        }
-        // end
     }
 } else {
     echo "Nothing";
