02313df4e26cf1c2ed5f729c5bdf5e846313eac3
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-26 11:24:00 +0100

    MAKING A SWITCH MOVE

--- script/REMODEL/restructure_loan_interest.php
+++ script/REMODEL/restructure_loan_interest.php
@@ -11,7 +11,7 @@ if (mysqli_num_rows($query_the_realaid) > 0) {
         $interest_rate = $q["interest_rate"];
         $repayment_date = $q["repayment_date"];
         // realaid_loan_interest_remodel SHOUT
-        $query_makeitup = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE interest_rate IS NULL");
+        $query_makeitup = mysqli_query($connection, "SELECT * FROM `realaid_loan_interest_remodel` WHERE interest_rate IS NULL AND status = '0'");
         if (mysqli_num_rows($query_makeitup) > 0) {
             // realaid
             while ($yu = mysqli_fetch_array($query_makeitup)) {
@@ -19,6 +19,7 @@ if (mysqli_num_rows($query_the_realaid) > 0) {
                 $query_delete_loanremodel = mysqli_query($connection, "DELETE FROM `loan_remodeling_reform` WHERE client_id = '$d_c_id'");
                 if ($query_delete_loanremodel) {
                     echo "DONE WITH DELETE";
+                    $query_update_realaid = mysqli_query($connection, "UPDATE `realaid_loan_interest_remodel` SET status = '3' WHERE id = '$qid'");
                 } else {
                     echo "ERROR ON DELETE";
                 }
