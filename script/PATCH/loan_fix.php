b241f50a56214f33775067af0aac41b3ab35e30f
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-30 12:05:06 +0100

    MAKING A MOVE

--- script/PATCH/loan_fix.php
+++ script/PATCH/loan_fix.php
@@ -2,7 +2,7 @@
 include("../../functions/connect.php");
 $int_id = '13';
 
-$query_loan_migrate = mysqli_query($connection, "SELECT * FROM `outstanding_report_migrate` WHERE migration_status = '2'");
+$query_loan_migrate = mysqli_query($connection, "SELECT * FROM `outstanding_report_migrate` WHERE migration_status = '3'");
 if (mysqli_num_rows($query_loan_migrate)) {
     while($qlm = mysqli_fetch_array($query_loan_migrate)) {
         // no principal loan
@@ -29,9 +29,22 @@ if (mysqli_num_rows($query_loan_migrate)) {
                 $new_interest = $interest / $get_com_inst;
                 $update_loan_repayment = mysqli_query($connection, "UPDATE `loan_repayment_schedule` SET installment = '1', principal_amount = '$new_principal', interest_amount = '$new_interest' WHERE loan_id = '$loan_id' AND int_id = '$int_id'");
                 if ($update_loan_repayment) {
-                    $update_migrate = mysqli_query($connection, "UPDATE `outstanding_report_migrate` SET  migration_status = '3' WHERE id = '$mid'");
+                    $update_migrate = mysqli_query($connection, "UPDATE `outstanding_report_migrate` SET  migration_status = '2' WHERE id = '$mid'");
                     if ($update_migrate) {
                         echo "...";
+                $query_get_PM = mysqli_query($connection, "SELECT SUM(principal_amount) AS p_m FROM `loan_repayment_schedule` WHERE loan_id = '$loan_id' AND int_id = '$int_id' AND installment = '1'");
+                $query_get_IM = mysqli_query($connection, "SELECT SUM(interest_amount) AS a_m FROM `loan_repayment_schedule` WHERE loan_id = '$loan_id' AND int_id = '$int_id' AND installment = '1'");
+                $poi = mysqli_fetch_array($query_get_PM);
+                $poi2 = mysqli_fetch_array($query_get_IM);
+                $out_p = $poi["p_m"];
+                $out_i = $poi2["i_m"];
+                $out_loan_bal = $out_p + $out_i;
+                        $update_loan_balance = mysqli_query($connection, "UPDATE `loan` SET total_outstanding_derived = '$out_loan_bal' WHERE id = '$loan_id' AND int_id = '$int_id'");
+                        if ($update_loan_balance) {
+                            echo 'bal'.$out_loan_bal;
+                        } else {
+                            echo "90";
+                        }
                     } else {
                         echo "BAD UPDATE";
                     }
