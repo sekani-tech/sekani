9505f3516be37b50e4b6406edae9b8d3e3d0f68d
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-10-30 11:40:23 +0100

    MAKINF MOVE

--- script/PATCH/loan_fix.php
+++ script/PATCH/loan_fix.php
@@ -2,15 +2,14 @@
 include("../../functions/connect.php");
 $int_id = '13';
 
-$query_loan_migrate = mysqli_query($connection, "SELECT * FROM `outstanding_report_migrate` WHERE migration_status = '1'");
+$query_loan_migrate = mysqli_query($connection, "SELECT * FROM `outstanding_report_migrate` WHERE migration_status = '2'");
 if (mysqli_num_rows($query_loan_migrate)) {
     while($qlm = mysqli_fetch_array($query_loan_migrate)) {
         // no principal loan
         $mid = $qlm["id"];
         $old_client_id = $qlm["client_id"];
         $interest = $qlm["interest"];
-        $installments = $qlm["installments"];
-        $outstanding_principal = $qlm["outstanding_principal"];
+        // $installments = $qlm["installments"];
         // NEXT MOVE
         $query_client = mysqli_query($connection, "SELECT * FROM `client` WHERE account_no = '$old_client_id' AND int_id = '$int_id'");
         if ($query_client) {
@@ -18,16 +17,19 @@ if (mysqli_num_rows($query_loan_migrate)) {
             $client_id = $qc["id"];
             // calculate the installment
             $query_loan = mysqli_query($connection, "SELECT * FROM `loan` WHERE client_id = '$client_id' AND int_id = '$int_id'");
-            if ($query_loan) {
-                $ql = mysqli_fetch_array($query_loan);
+            if (mysqli_num_rows($query_loan) > 0) {
+                while ($ql = mysqli_fetch_array($query_loan)) {
                 $loan_id = $ql["id"];
+                $approved_principal = $ql["approved_principal"];
                 // calculate the loan int and prin
-                $new_principal = $outstanding_principal / $installments;
-                $new_interest = $interest / $installments;
                 // update
+                $query_get_Repayment = mysqli_query($connection, "SELECT * FROM `loan_repayment_schedule` WHERE loan_id = '$loan_id' AND int_id = '$int_id'");
+                $get_com_inst = mysqli_num_rows($query_get_Repayment);
+                $new_principal = ($approved_principal / $get_com_inst);
+                $new_interest = $interest / $get_com_inst;
                 $update_loan_repayment = mysqli_query($connection, "UPDATE `loan_repayment_schedule` SET installment = '1', principal_amount = '$new_principal', interest_amount = '$new_interest' WHERE loan_id = '$loan_id' AND int_id = '$int_id'");
                 if ($update_loan_repayment) {
-                    $update_migrate = mysqli_query($connection, "UPDATE `outstanding_report_migrate` SET  migration_status = '2' WHERE id = '$mid'");
+                    $update_migrate = mysqli_query($connection, "UPDATE `outstanding_report_migrate` SET  migration_status = '3' WHERE id = '$mid'");
                     if ($update_migrate) {
                         echo "...";
                     } else {
@@ -35,6 +37,7 @@ if (mysqli_num_rows($query_loan_migrate)) {
                     }
                 } else {
                     echo "Wrong Repayment query";
+                }
                 }
             } else {
                 echo "BAD LOAN QUERY";
