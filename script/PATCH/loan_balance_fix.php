8d8fe34a966dcab86feedbbda9dab26fd24668a0
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2020-11-02 14:52:09 +0100

    THE CODES

--- script/PATCH/loan_balance_fix.php
+++ script/PATCH/loan_balance_fix.php
@@ -8,19 +8,24 @@ if (mysqli_num_rows($query_client_ln) > 0)  {
     while ($rx = mysqli_fetch_array($query_client_ln)) {
         $l_id = $rx["id"];
         $new_client_id = $rx["client_id"];
+        $total = $rx["total"];
         // next
         $query_loan = mysqli_query($connection, "SELECT * FROM `loan` WHERE client_id = '$new_client_id' AND int_id = '$int_id'");
         if (mysqli_num_rows($query_loan) > 0) {
             while ($uq = mysqli_fetch_array($query_loan)) {
                 $loan_id = $uq["id"];
-                $total = $uq["principal_amount"];
                 // put
-                    $update_disbursment_cache = mysqli_query($connection, "UPDATE `loan_disbursement_cache` SET principal_amount_proposed = '$total', principal_amount = '$total', approved_principal = '$total' WHERE client_id = '$new_client_id' AND int_id = '$int_id'");
+                $update_loan = mysqli_query($connection, "UPDATE `loan` SET principal_amount_proposed = '$total', principal_amount = '$total', approved_principal = '$total' WHERE id = '$loan_id'");
+                if ($update_loan) {
+                    $update_disbursment_cache = mysqli_query($connection, "UPDATE `loan_disbursement_cache` SET principal_amount_proposed = '$total', principal_amount = '$total', approved_principal = '$total' WHERE client_id = '$new_client_id'");
                     if ($update_disbursment_cache) {
                         echo "CAHCE";
                     } else {
                         echo "///g";
                     }
+                } else {
+                    echo "//";
+                }
             }
         } else {
             echo "NOT RUNNING LOAN";
