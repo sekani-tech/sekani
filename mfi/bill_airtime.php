c633749666ee72ae404ccacb928ecbd160cc5fe5
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2020-11-06 11:39:55 +0100

    Routes uis

--- mfi/bill_airtime.php
+++ mfi/bill_airtime.php
@@ -11,50 +11,39 @@ $destination = "../index.php";
     <div class="content">
         <div class="container-fluid">
           <!-- your content here -->
-          <div class="row">
-            <div class="col-md-12">
-              <div class="card">
+          <<div class="card">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Bills & Airtime</h4>
+                  <h4 class="card-title text-center">Bills & Airtime</h4>
+                  <p class="card-category text-center"></p>
+                </div>
+    </div>
+
+          <div class="row">
+            <div class="col-md-6 ml-auto mr-auto">
+              <div class="card card-pricing bg-primary"><div class="card-body ">
                   
-                  <!-- Insert number users institutions -->
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                  <table class="rtable display nowrap" style="width:100%">
-                      <thead class=" text-primary">
-                        <tr>
-                          <th>sn</th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <th></th>
-                          <th>Bills Payment</th>
-                          <th>Pay Bills - Electricity, Cable Tv e.t.c </th>
-                          <td><a href="bill.php" class="btn btn-info"><i class="material-icons" style="margin:auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          <th></th>
-                          <th>Airtime and Data</th>
-                          <th> Recharge Airtime and Data.</th>
-                          <td><a href="airtime.php" class="btn btn-info"><i class="material-icons" style="margin:auto;">description</i></a></td>
-                        </tr>
-                        <!-- <tr>
-                          <th>Transaction Pin</th>
-                          <th> Generate or Update your Bill & Airtime Transaction pin.</th>
-                          <td><a href="bill_pin.php" class="btn btn-info"><i class="material-icons">description</i></a></td>
-                        </tr> -->
-                      </tbody>
-                    </table>
+                  <h4 class="card-title">Bills Payment</h4>
+                  <p class="card-description">
+                  Pay Bills - Electricity, Cable Tv e.t.c 
+                  </p>
+                  <a href="bill.php" class="btn btn-white btn-round">View</a>
                   </div>
-                </div>
               </div>
             </div>
-          </div>
+
+            <div class="col-md-6 ml-auto mr-auto">
+              <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Airtime and Data</h4>
+                  <p class="card-description">
+                  Recharge Airtime and Data.
+                  </p>
+                  <a href="airtime.php" class="btn btn-white btn-round">View</a>
+                  </div>
+              </div>
+            </div>   
+
+        </div>
         </div>
       </div>
 
--- mfi/configuration.php
+++ mfi/configuration.php
@@ -11,92 +11,143 @@ $destination = "../index.php";
     <div class="content">
         <div class="container-fluid">
           <!-- your content here -->
-          <div class="row">
-            <div class="col-md-12">
-              <div class="card">
+          <div class="card">
                 <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Configuration</h4>
-              
-                  <!-- Insert number users institutions -->
+                  <h4 class="card-title text-center">Configuration</h4>
+                  <p class="card-category text-center"></p>
                 </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                  <table class="rtable display nowrap" style="width:100%">
-                      <thead class=" text-primary">
-                        <tr>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                          <th></th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                      <tr>
-                          
-                          <th>Asset Depreciation</th>
-                          <th>Setup the Depreciation Value for the Assets in the institution</th>
-                          <td><a href="dep_setup.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th>Products</th>
-                          <th>Add, edit and update the Loan products of the instution</th>
-                          <td><a href="products_config.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th>Staff Management</th>
-                          <th>Manage all the staff activities and their status</th>
-                          <td><a href="staff_mgmt.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th>Branch</th>
-                          <th>View and create institution branches</th>
-                          <td><a href="branch.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th>SMS Campaign</th>
-                          <th>Handle the alerts that the system recieves</th>
-                          <td><a href="alert.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <!-- <tr>
-                          <th>General Ledger Template</th>
-                          <th>Assign General Ledgers to system posting activities</th>
-                          <td><a href="gl_template.php" class="btn btn-info"><i class="material-icons">description</i></a></td>
-                        </tr> -->
-                        <tr>
-                          
-                          <th> <b> SEKANI WALLET </b></th>
-                          <th>Fund Institution Sekani Wallet, Keep Activity Up to Date</th>
-                          <td><a href="sekani_wallet.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th> <b> Loan Reconciliation </b></th>
-                          <th>Edit and make sure the loan balances match at the end of a particular accounting period.</th>
-                          <td><a href="loan_reconciliation.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th> <b> Track Dormancy </b></th>
-                          <th>Keep track of all Accounts that have not been in use</th>
-                          <td><a href="trk_dormant.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                        <tr>
-                          
-                          <th> <b> Bulk Update </b></th>
-                          <th>Upload Bulk data from institution database</th>
-                          <td><a href="bulk_update.php" class="btn btn-info"><i class="material-icons" style="margin: auto;">description</i></a></td>
-                        </tr>
-                      </tbody>
-                    </table>
+    </div>
+
+          <div class="row">
+          <div class="col-md-4 ml-auto mr-auto">
+              <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Asset Depreciation</h4>
+                  <p class="card-description">
+                  Setup the Depreciation Value for the Assets in the institution
+                  </p>
+                  <a href="dep_setup.php" class="btn btn-white btn-round">View</a>
                   </div>
-                </div>
               </div>
             </div>
-          </div>
+            
+
+            <div class="col-md-4 ml-auto mr-auto">
+            <div class="card card-pricing bg-primary" ><div class="card-body ">
+                  
+                  <h4 class="card-title">Products</h4>
+                  <p class="card-description">
+                  Add, edit and update the Loan products of the instution
+                  </p>
+                  
+                  <a href="products_config.php" class="btn btn-white btn-round" style="margin-top: 30px;">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            <div class="col-md-4 ml-auto mr-auto">
+
+            <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Staff Management</h4>
+                  <p class="card-description">
+                  Manage all the staff activities and their status
+                  </p>
+                  <a href="staff_mgmt.php" class="btn btn-white btn-round" style="margin-top: 30px;">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            
+
+        </div>         
+
+        <div class="row">
+          <div class="col-md-4 ml-auto mr-auto">
+              <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Branch</h4>
+                  <p class="card-description">
+                  View and create institution branches
+                  </p>
+                  <a href="branch.php" class="btn btn-white btn-round">View</a>
+                  </div>
+              </div>
+            </div>
+            
+
+            <div class="col-md-4 ml-auto mr-auto">
+            <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">SMS Campaign</h4>
+                  <p class="card-description">
+                  Handle the alerts that the system recieves
+                  </p>
+                  <a href="alert.php" class="btn btn-white btn-round">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            <div class="col-md-4 ml-auto mr-auto">
+
+            <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">SEKANI WALLET</h4>
+                  <p class="card-description">
+                  Fund Institution Sekani Wallet, Keep Activity Up to Date
+                  </p>
+                  <a href="sekani_wallet.php" class="btn btn-white btn-round">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            
+
+        </div>
+
+
+        <div class="row">
+          <div class="col-md-4 ml-auto mr-auto">
+              <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Loan Reconciliation</h4>
+                  <p class="card-description">
+                  Edit and make sure the loan balances match at the end of a particular accounting period.
+                  </p>
+                  <a href="loan_reconciliation.php" class="btn btn-white btn-round">View</a>
+                  </div>
+              </div>
+            </div>
+            
+
+            <div class="col-md-4 ml-auto mr-auto">
+            <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Track Dormancy</h4>
+                  <p class="card-description">
+                  Keep track of all Accounts that have not been in use
+                  </p>
+                  <a href="trk_dormant.php" class="btn btn-white btn-round" style="margin-top: 30px;">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            <div class="col-md-4 ml-auto mr-auto">
+
+            <div class="card card-pricing bg-primary"><div class="card-body ">
+                  
+                  <h4 class="card-title">Bulk Update</h4>
+                  <p class="card-description">
+                  Upload Bulk data from institution database
+                  </p>
+                  <a href="bulk_update.php" class="btn btn-white btn-round" style="margin-top: 30px;">View</a>
+                  </div>
+              </div>
+            </div>   
+
+            
+
+        </div>
         </div>
       </div>
 
