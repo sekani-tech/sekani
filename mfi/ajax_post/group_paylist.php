1901a4ccef62bd0bc5f667d36a3ac0fd2b196688
Samuel <manjigs@gmail.com>
2020-11-18 16:07:13 +0100

    group posting account numbers drop

--- mfi/ajax_post/group_paylist.php
+++ mfi/ajax_post/group_paylist.php
@@ -18,28 +18,49 @@ if (isset($_POST["id"])) {
         $cl_id = $groupClient['client_id'];
 //        selecting from account with clients Account Number
 
-        $accountBalCont = "00".$groupClient['account_no'];
-        $accountBal = selectOne('account', ['account_no' => $accountBalCont]);
-        $product_type = $accountBal["product_id"];
+        // $accountBalCont = "00".$groupClient['account_no'];
+        
+        // $accountBal = selectAll('account', ['client_id' => $cl_id]);
+        // var_dump($accountBal);
+        // $product_type = $accountBal["product_id"];
 
 //        get user account details
-        $savingProductCon = ['id' => $product_type];
-        $savingProduct = selectOne('savings_product', $savingProductCon);
-//        dd($savingProduct);
-        $accountNumb_name = $savingProduct['name'];
+//         $savingProductCon = ['id' => $product_type];
+//         $savingProduct = selectOne('savings_product', $savingProductCon);
+// //        dd($savingProduct);
+//         $accountNumb_name = $savingProduct['name'];
 //        $accountNumb_id = $accountBalCont['id'];
+
+        function fill_account($connection) {
+            $int_id = $_SESSION['int_id'];
+            global $cl_id;
+            $client_id = $cl_id;
+            $outer = '';
+            $pen = "SELECT * FROM account WHERE client_id = '$client_id'";
+            $res = mysqli_query($connection, $pen);
+            while ($row = mysqli_fetch_array($res))
+            {
+            $product_type = $row["product_id"];
+            $get_product = mysqli_query($connection, "SELECT * FROM savings_product WHERE id = '$product_type' AND int_id = '$int_id'");
+            while ($mer = mysqli_fetch_array($get_product)) {
+            $p_n = $mer["name"];
+            $outer .= '<option value="'.$row["id"].'">'.$row["account_no"].' - '.$p_n.'</option>';
+            }
+            }
+            return $outer;
+        }
+
 
 
         $out = '
         <tr>
         <td class="text-center">' . $i . '</td>
         <td>' . $c_name . '</td>
-        <td>' . $accountBal['account_balance_derived'] . '</td>
+        <td></td>
         <td>
-        <select id="account" name="account[]" class="form-control" required>
-        <option value="">Select An Account</option>
-        <option value="'.$accountBalCont.'">' . $accountBalCont . ' - ' . $accountNumb_name . '</option>
-</select></td>
+            <select id="account" name="account[]" class="form-control" required>
+                '.fill_account($connection).'
+            </select></td>
         <td></td>
         <td>
             <input type ="text" hidden name="customerID[]" value="' . $cl_id . '"  >
