bfe85ad044b3e8866d8109c03cb28eda30962fdb
Samuel <manjigs@gmail.com>
2020-12-03 11:23:00 +0100

    Client table readresss

--- mfi/client.php
+++ mfi/client.php
@@ -1,10 +1,11 @@
 <?php
-
 $page_title = "Clients";
 $destination = "index.php";
 include("header.php");
 $br_id = $_SESSION['branch_id'];
 
+?>
+<?php
 //  Sweet alert Function
 
 // If it is successfull, It will show this message
@@ -27,7 +28,8 @@ if (isset($_GET["message1"])) {
     ';
         $_SESSION["lack_of_intfund_$key"] = 0;
     }
-} // If it is not successfull, It will show this message
+}
+// If it is not successfull, It will show this message
 else if (isset($_GET["message2"])) {
     $key = $_GET["message2"];
     $tt = 0;
@@ -69,7 +71,7 @@ if (isset($_GET["message3"])) {
     }
 } else if (isset($_GET["message4"])) {
     $key = $_GET["message4"];
-// $out = $_SESSION["lack_of_intfund_$key"];
+    // $out = $_SESSION["lack_of_intfund_$key"];
     $tt = 0;
     if ($tt !== $_SESSION["lack_of_intfund_$key"]) {
         echo '<script type="text/javascript">
@@ -107,60 +109,33 @@ $(document).ready(function(){
     }
 }
 ?>
-    <!-- Content added here -->
-    <div class="content">
-        <div class="container-fluid">
-            <!-- your content here -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header card-header-primary">
-                            <h4 class="card-title ">Clients</h4>
-                            <p class="card-category"><?php
-                                $query = "SELECT client.id, 
-                                    client.BVN, 
-                                    client.date_of_birth, 
-                                    client.gender, 
-                                    client.account_type, 
-                                    client.account_no, 
-                                    client.mobile_no, 
-                                    client.firstname, 
-                                    client.lastname,  
-                                    staff.first_name, 
-                                    staff.last_name 
-                                    FROM client 
-                                    JOIN staff ON client.loan_officer_id = staff.id 
-                                    WHERE client.int_id = '$sessint_id'
-                                    && client.status = 'Approved'";
-                                $result = mysqli_query($connection, $query);
-                                $inr = mysqli_num_rows($result);
-                                echo $inr;
-                                ?> registered clients || <a style="color: white;" href="manage_client.php">Create New
-                                    client</a></p>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="rtable display nowrap" style="width:100%">
-                                    <thead class=" text-primary">
-                                    <?php
-                                    $query = "SELECT client.id, 
-                                        client.BVN, 
-                                        client.date_of_birth, 
-                                        client.gender, 
-                                        client.account_type, 
-                                        client.account_no, 
-                                        client.mobile_no, 
-                                        client.firstname, 
-                                        client.lastname,  
-                                        staff.first_name, 
-                                        staff.last_name 
-                                        FROM client JOIN staff ON 
-                                        client.loan_officer_id = staff.id 
-                                        WHERE client.int_id = '$sessint_id'
-                                        && client.status = 'Approved'";
-                                    $result = mysqli_query($connection, $query);
-                                    ?>
-                                    <th></th>
+<!-- Content added here -->
+<link href='datatable/DataTables/datatables.min.css' rel='stylesheet' type='text/css'>
+<script src="datatable/DataTables/datatables.min.js"></script>
+<div class="content">
+    <div class="container-fluid">
+        <!-- your content here -->
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header card-header-primary">
+                        <h4 class="card-title ">Clients</h4>
+                        <p class="card-category">
+                            <?php
+                            $query = "SELECT COUNT(firstname) FROM client WHERE int_id = '$sessint_id' AND status = 'Approved'";
+                            $result = mysqli_query($connection, $query);
+                            if ($result) {
+                                $inr = mysqli_fetch_array($result);
+                                echo $inr['COUNT(firstname)'];
+                            }
+                            ?> registered clients || <a style="color: white;" href="manage_client.php">Create New client</a></p>
+
+                    </div>
+                    <div class="card-body">
+                        <!-- end search -->
+                        <div class="table-responsive">
+                            <table id="empTable" class="display nowrap dataTable" style="width:100%">
+                                <thead class="text-primary">
                                     <th>
                                         First Name
                                     </th>
@@ -179,98 +154,52 @@ $(document).ready(function(){
                                     <th>View</th>
                                     <th>Close</th>
                                     <!-- <th>Phone</th> -->
-                                    </thead>
-                                    <tbody>
-                                    <?php if (mysqli_num_rows($result) > 0) {
-                                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) { ?>
-                                            <tr>
-                                                <?php $row["id"]; ?>
-                                                <th></th>
-                                                <th><?php echo $row["firstname"]; ?></th>
-                                                <th><?php echo $row["lastname"]; ?></th>
-                                                <th><?php echo strtoupper($row["first_name"] . " " . $row["last_name"]); ?></th>
-                                                <?php
-                                                $class = "";
-                                                $row["account_type"];
-                                                $cid = $row["id"];
-                                                $atype = mysqli_query($connection, "SELECT * FROM account WHERE client_id = '$cid'");
-                                                if (count([$atype]) == 1) {
-                                                    $yxx = mysqli_fetch_array($atype);
-                                                    if (isset($yxx['product_id'])) {
-                                                        $actype = $yxx['product_id'];
-                                                    }
-                                                    $spn = mysqli_query($connection, "SELECT * FROM savings_product WHERE id = '$actype' AND int_id = '$sessint_id'");
-                                                    if (count([$spn])) {
-                                                        $d = mysqli_fetch_array($spn);
-                                                        if (isset($d["name"])) {
-                                                            $savingp = $d["name"];
-                                                        }
-                                                    }
-                                                }
-                                                ?>
-                                                <th>
-                                                    <?php
-                                                    if ($row['account_type'] == "") {
-                                                        echo $row['account_type'];
-                                                    } else {
-                                                        echo $savingp;
-                                                    }
-                                                    ?>
-                                                </th>
-                                                <?php
-                                                $get_one_account = mysqli_query($connection, "SELECT * FROM `account` WHERE client_id = '$cid' ORDER BY id ASC LIMIT 1");
-                                                if (mysqli_num_rows($get_one_account) == 1) {
-                                                    $rowa = mysqli_fetch_array($get_one_account);
-                                                    $soc = $rowa["account_no"];
-                                                } else {
-                                                    $soc = "No Account";
-                                                }
+                                </thead>
+                                <!-- refresh
+                      end refresh -->
+                            </table>
 
-                                                $length = strlen($soc);
-                                                if ($length == 1) {
-                                                    $acc = "000000000" . $soc;
-                                                } elseif ($length == 2) {
-                                                    $acc = "00000000" . $soc;
-                                                } elseif ($length == 3) {
-                                                    $acc = "00000000" . $soc;
-                                                } elseif ($length == 4) {
-                                                    $acc = "0000000" . $soc;
-                                                } elseif ($length == 5) {
-                                                    $acc = "000000" . $soc;
-                                                } elseif ($length == 6) {
-                                                    $acc = "0000" . $soc;
-                                                } elseif ($length == 7) {
-                                                    $acc = "000" . $soc;
-                                                } elseif ($length == 8) {
-                                                    $acc = "00" . $soc;
-                                                } elseif ($length == 9) {
-                                                    $acc = "0" . $soc;
-                                                } elseif ($length == 10) {
-                                                    $acc = $rowa["account_no"];
-                                                } else {
-                                                    $acc = $rowa["account_no"];
-                                                }
-                                                ?>
-                                                <th><?php echo $acc; ?></th>
-                                                <td><a href="client_view.php?edit=<?php echo $row["id"]; ?>"
-                                                       class="btn btn-info">View</a></td>
-                                                <td>
-                                                    <a href="../functions/close_client.php?edit=<?php echo $row["id"]; ?>"
-                                                       class="btn btn-info">Close</a></td>
-                                            </tr>
-                                        <?php }
-                                    }
-                                    ?>
-                                    <!-- <th></th> -->
-                                    </tbody>
-                                </table>
-                            </div>
                         </div>
+                        <script>
+                            $(document).ready(function() {
+                                $('#empTable').DataTable({
+                                    'processing': true,
+                                    'serverSide': true,
+                                    'serverMethod': 'post',
+                                    'ajax': {
+                                        'url': 'datatable/ajaxfile.php'
+                                    },
+                                    'columns': [{
+                                            data: 'firstname'
+                                        },
+                                        {
+                                            data: 'lastname'
+                                        },
+                                        {
+                                            data: 'account_officer'
+                                        },
+                                        {
+                                            data: 'account_type'
+                                        },
+                                        {
+                                            data: 'account_no'
+                                        },
+                                        {
+                                            data: 'view'
+                                        },
+                                        {
+                                            data: 'close'
+                                        },
+                                    ]
+                                });
+                            });
+                        </script>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
 <?php
 include("footer.php");
