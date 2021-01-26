190e2f7e9decb57c198a839495dd479b9c57a2a2
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2021-01-26 11:19:24 +0100

    client_transfer & create_ftd & staff_transfer

---
+++ mfi/create_ftd.php
@@ -0,0 +1,19 @@
+<?php
+
+$page_title = "Create FTD Product";
+$destination = "index.php";
+include("header.php");
+
+?>
+
+<div class="content">
+        <div class="container-fluid">
+          <!-- your content here -->
+        </div>
+      </div>
+
+<?php
+
+include("footer.php");
+
+?>
