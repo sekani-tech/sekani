7ae3713d454f5c90886f859aff152adc6dc923a3
Pinza studio <67124011+dev-pinza@users.noreply.github.com>
2021-01-26 15:01:58 +0100

    Updated Loan Balance CODE
    2021-01-26

--- loan_repayment/update_loan_balance.php
+++ loan_repayment/update_loan_balance.php
@@ -8,7 +8,7 @@
 include('../functions/connect.php');
 // col name total_outstanding_derived
 // Select all loan where outstanding loan balance is greater than zero
-$query_loan_table = mysqli_query($connection, "SELECT * FROM `loan` WHERE total_outstanding_derived > 0");
+$query_loan_table = mysqli_query($connection, "SELECT * FROM `loan`");
 if (mysqli_num_rows($query_loan_table) > 0) {
     while ($row = mysqli_fetch_array($query_loan_table)) {
         $loan_id = $row["id"];
@@ -18,7 +18,7 @@ if (mysqli_num_rows($query_loan_table) > 0) {
         $outstanding_loan_repayment_sch = $rowx["total_loan_sch"];
 //  sum all balances in loan arrears with loan id where installment is 1.
         $query_sum_loan_arrears = mysqli_query($connection, "SELECT SUM(principal_amount + interest_amount) AS total_loan_arr FROM `loan_arrear` WHERE loan_id = '$loan_id' AND installment = '1'");
-        $rowy = mysqli_fetch_array($query_sum_loan_repayment_sch);
+        $rowy = mysqli_fetch_array($query_sum_loan_arrears);
         $outstanding_loan_arrears = $rowy["total_loan_arr"];
 // add balances together and update loan table
         $total_outstanding_derived = $outstanding_loan_repayment_sch + $outstanding_loan_arrears;
