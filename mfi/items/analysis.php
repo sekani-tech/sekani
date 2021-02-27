68064868e86230ed86f4a88b6ecb5e2847419513
Nylat119 <36956517+Nylat119@users.noreply.github.com>
2021-02-27 20:34:22 +0100

    loan analysis report update

--- mfi/items/analysis.php
+++ mfi/items/analysis.php
@@ -152,115 +152,122 @@
         $sector11 = mysqli_num_rows($donk);
     }
 
-    $ttunt = $amount1 + $amount2 + $amount3 + $amount4 + $amount5 + $amount6 + $amount7 + $amount8 + $amount9 + $amount10 + $amount11;
     $ttlsector = $sector1 + $sector2 + $sector3 + $sector4 + $sector5 + $sector6 + $sector7 + $sector8 + $sector9 + $sector10 + $sector11;
+    $ttunt = $amount1 + $amount2 + $amount3 + $amount4 + $amount5 + $amount6 + $amount7 + $amount8 + $amount9 + $amount10 + $amount11;
+
+    $percentAmount1 = !empty($amount1) ? $amount1 / $ttunt * 100 : '';
+    $percentAmount2 = !empty($amount2) ? $amount2 / $ttunt * 100 : '';
+    $percentAmount3 = !empty($amount3) ? $amount3 / $ttunt * 100 : '';
+    $percentAmount4 = !empty($amount4) ? $amount4 / $ttunt * 100 : '';
+    $percentAmount5 = !empty($amount5) ? $amount5 / $ttunt * 100 : '';
+    $percentAmount6 = !empty($amount6) ? $amount6 / $ttunt * 100 : '';
+    $percentAmount7 = !empty($amount7) ? $amount7 / $ttunt * 100 : '';
+    $percentAmount8 = !empty($amount8) ? $amount8 / $ttunt * 100 : '';
+    $percentAmount9 = !empty($amount9) ? $amount9 / $ttunt * 100 : '';
+    $percentAmount10 = !empty($amount10) ? $amount10 / $ttunt * 100 : '';
+    $percentAmount11 = !empty($amount11) ? $amount11 / $ttunt * 100 : '';
+
     $ttlamount = number_format($ttunt, 2);
 
     $out ='
     `<div class="card">
-      <div class="card-header card-header-primary">
-        <h4 class="card-title">Sectoral Analysis of Loans and Advances</h4>
-      </div>
-      <div class="card-body">
-      <div class="form-group">
-      <form method = "POST" action = "../composer/loan_analysis.php">
-        <input hidden name ="branch_id" type="text" value="'.$branch_id.'"/>
-        <input hidden name ="start" type="text" value="'.$start.'"/>
-        <input hidden name ="end" type="text" value="'.$end.'"/>
-        <button type="submit" id="disbursed" class="btn btn-primary pull-left">Download PDF</button>
-      </form>
-      </div>
-        <table class="table">
-          <thead>
-            <th style="font-weight:bold;">SECTOR</th>
-            <th style="font-weight:bold; text-align: center;">NUMBER OF LOANS</th>
-            <th style="font-weight:bold; text-align: center;">AMOUNT <br> &#x20A6</th>
-            <th style="font-weight:bold; text-align: center;">%</th>
-          </thead>
-          <tbody>
-              <tr>
-                  <td>Agriculture, Mining & Quarry</td>
-                  <td style="text-align: center;">'.$sector1.'</td>
-                  <td style="text-align: center;">'.$amount1.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Manufacturing</td>
-                  <td style="text-align: center;">'.$sector2.'</td>
-                  <td style="text-align: center;">'.$amount2.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Agricultural sector</td>
-                  <td style="text-align: center;">'.$sector3.'</td>
-                  <td style="text-align: center;">'.$amount3.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Banking</td>
-                  <td style="text-align: center;">'.$sector4.'</td>
-                  <td style="text-align: center;">'.$amount4.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Public Service</td>
-                  <td style="text-align: center;">'.$sector5.'</td>
-                  <td style="text-align: center;">'.$amount5.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Health</td>
-                  <td style="text-align: center;">'.$sector6.'</td>
-                  <td style="text-align: center;">'.$amount6.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Education</td>
-                  <td style="text-align: center;">'.$sector7.'</td>
-                  <td style="text-align: center;">'.$amount7.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Tourism</td>
-                  <td style="text-align: center;">'.$sector8.'</td>
-                  <td style="text-align: center;">'.$amount8.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Civil Service</td>
-                  <td style="text-align: center;">'.$sector9.'</td>
-                  <td style="text-align: center;">'.$amount9.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Trade & Commerce</td>
-                  <td style="text-align: center;">'.$sector10.'</td>
-                  <td style="text-align: center;">'.$amount10.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td>Others</td>
-                  <td style="text-align: center;">'.$sector11.'</td>
-                  <td style="text-align: center;">'.$amount11.'</td>
-                  <td style="background-color:bisque;"></td>
-              </tr>
-              <tr>
-                  <td><b>TOTAL</b></td>
-                  <th style="text-align: center; background-color:bisque;"><b>'.$ttlsector.'</b></th>
-                  <th style="text-align: center; background-color:bisque;"><b>'.$ttlamount.'</b></th>
-                  <th style="text-align: center; background-color:bisque;"><b></b></th>
-              </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>`
-    <!--//report ends here -->
-    <!--<div class="card">
-        <div class="card-body">
-        <a href="" class="btn btn-primary">Back</a>
-        <a href="" class="btn btn-success btn-left">Print</a>
+        <div class="card-header card-header-primary">
+            <h4 class="card-title">Sectoral Analysis of Loans and Advances</h4>
         </div>
-        </div> -->
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <th style="font-weight:bold;">SECTOR</th>
+                    <th style="font-weight:bold; text-align: center;">NUMBER OF LOANS</th>
+                    <th style="font-weight:bold; text-align: center;">AMOUNT <br> &#x20A6</th>
+                    <th style="font-weight:bold; text-align: center;">%</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Agriculture, Mining & Quarry</td>
+                        <td style="text-align: center;">'.$sector1.'</td>
+                        <td style="text-align: center;">'.$amount1.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount1.'</td>
+                    </tr>
+                    <tr>
+                        <td>Manufacturing</td>
+                        <td style="text-align: center;">'.$sector2.'</td>
+                        <td style="text-align: center;">'.$amount2.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount2.'</td>
+                    </tr>
+                    <tr>
+                        <td>Agricultural sector</td>
+                        <td style="text-align: center;">'.$sector3.'</td>
+                        <td style="text-align: center;">'.$amount3.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount3.'</td>
+                    </tr>
+                    <tr>
+                        <td>Banking</td>
+                        <td style="text-align: center;">'.$sector4.'</td>
+                        <td style="text-align: center;">'.$amount4.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount4.'</td>
+                    </tr>
+                    <tr>
+                        <td>Public Service</td>
+                        <td style="text-align: center;">'.$sector5.'</td>
+                        <td style="text-align: center;">'.$amount5.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount5.'</td>
+                    </tr>
+                    <tr>
+                        <td>Health</td>
+                        <td style="text-align: center;">'.$sector6.'</td>
+                        <td style="text-align: center;">'.$amount6.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount6.'</td>
+                    </tr>
+                    <tr>
+                        <td>Education</td>
+                        <td style="text-align: center;">'.$sector7.'</td>
+                        <td style="text-align: center;">'.$amount7.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount7.'</td>
+                    </tr>
+                    <tr>
+                        <td>Tourism</td>
+                        <td style="text-align: center;">'.$sector8.'</td>
+                        <td style="text-align: center;">'.$amount8.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount8.'</td>
+                    </tr>
+                    <tr>
+                        <td>Civil Service</td>
+                        <td style="text-align: center;">'.$sector9.'</td>
+                        <td style="text-align: center;">'.$amount9.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount9.'</td>
+                    </tr>
+                    <tr>
+                        <td>Trade & Commerce</td>
+                        <td style="text-align: center;">'.$sector10.'</td>
+                        <td style="text-align: center;">'.$amount10.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount10.'</td>
+                    </tr>
+                    <tr>
+                        <td>Others</td>
+                        <td style="text-align: center;">'.$sector11.'</td>
+                        <td style="text-align: center;">'.$amount11.'</td>
+                        <td style="background-color:bisque; text-align: center;">'.$percentAmount11.'</td>
+                    </tr>
+                    <tr>
+                        <td><b>TOTAL</b></td>
+                        <th style="text-align: center; background-color:bisque;"><b>'.$ttlsector.'</b></th>
+                        <th style="text-align: center; background-color:bisque;"><b>'.$ttlamount.'</b></th>
+                        <th style="text-align: center; background-color:bisque;"><b></b></th>
+                    </tr>
+                </tbody>
+            </table>
+
+            <div class="form-group mt-4">
+                <form method = "POST" action = "../composer/loan_analysis.php">
+                    <input hidden name ="branch_id" type="text" value="'.$branch_id.'"/>
+                    <input hidden name ="start" type="text" value="'.$start.'"/>
+                    <input hidden name ="end" type="text" value="'.$end.'"/>
+                    <button type="submit" id="disbursed" class="btn btn-primary pull-left">Download PDF</button>
+                </form>
+            </div>
+        </div>
+    </div>`
     ';
     echo $out;
 }
