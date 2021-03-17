b69cd9d3dd7eadccf5c34ca0c2043d8ec92c4e12
ose-me <oseeronlan@gmail.com>
2021-03-17 16:37:58 +0100

    [feat]:autocomplete on disburse loan

--- mfi/lend.php
+++ mfi/lend.php
@@ -6,6 +6,55 @@ include("header.php");
 include("ajaxcallx.php");
 
 ?>
+<style>
+* { box-sizing: border-box; }
+body {
+  font: 16px Arial;
+}
+.autocomplete {
+  /*the container must be positioned relative:*/
+  position: relative;
+  display: inline-block;
+}
+input {
+  border: 1px solid transparent;
+  background-color: #f1f1f1;
+  padding: 10px;
+  font-size: 16px;
+}
+input[type=text] {
+    background-color: transparent;
+    width: 100%;
+    border: 0;
+    border-bottom: 1px solid #d5d0d0;
+}
+.autocomplete-items {
+  position: absolute;
+  border: 1px solid #d4d4d4;
+  border-bottom: none;
+  border-top: none;
+  z-index: 99;
+  /*position the autocomplete items to be the same width as the container:*/
+  top: 100%;
+  left: 0;
+  right: 0;
+}
+.autocomplete-items div {
+  padding: 10px;
+  cursor: pointer;
+  background-color: #fff;
+  border-bottom: 1px solid #d4d4d4;
+}
+.autocomplete-items div:hover {
+  /*when hovering an item:*/
+  background-color: #e9e9e9;
+}
+.autocomplete-active {
+  /*when navigating through the items using the arrow keys:*/
+  background-color: DodgerBlue !important;
+  color: #ffffff;
+}
+</style>
 <!-- Content added here -->
 <div class="content">
     <div class="container-fluid">
@@ -135,7 +184,9 @@ include("ajaxcallx.php");
                         <p class="card-category">Fill in all important data</p>
                     </div>
                     <div class="card-body">
-                        <form id="form" action="../functions/int_lend_upload.php" method="POST">
+
+                    <!-- autocomplete turned off in form -->
+                        <form id="form" autocomplete="off" action="../functions/int_lend_upload.php" method="POST">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
@@ -160,12 +211,21 @@ include("ajaxcallx.php");
                                             {
                                                 $sint_id = $_SESSION["int_id"];
                                                 $branc = $_SESSION["branch_id"];
-                                                $org = "SELECT client.id, client.firstname, client.lastname, client.middlename FROM client JOIN branch ON client.branch_id = branch.id WHERE client.int_id = '$sint_id' AND (branch.id = '$branc' OR branch.parent_id = '$branc') AND status = 'Approved' ORDER BY firstname ASC";
+                                                $org = "SELECT client.id, client.display_name FROM client JOIN branch ON client.branch_id = branch.id WHERE client.int_id = '$sint_id' AND (branch.id = '$branc' OR branch.parent_id = '$branc') AND status = 'Approved' ORDER BY firstname ASC";
                                                 $res = mysqli_query($connection, $org);
                                                 $out = '';
+                                                $rows_length = mysqli_num_rows($res);
+                                                $i = 0;
                                                 while ($row = mysqli_fetch_array($res)) {
-                                                    $out .= '<option value="' . $row["id"] . '">' . strtoupper($row["firstname"]) . " " . strtoupper($row["middlename"]) . " " . strtoupper($row["lastname"]) . '</option>';
+                                                    $i++;
+                                                    if($i < $rows_length) {
+                                                        $out .= $row['id'] . '-' . $row['display_name'] . ',';
+                                                    } else {
+                                                        $out .= $row['id'] . '-' . $row['display_name'];
+                                                    }
+
                                                 }
+
                                                 return $out;
                                             }
 
@@ -182,6 +242,7 @@ include("ajaxcallx.php");
                                                 return $out;
                                             }
 
+                                            
                                             // Function for charges
                                             function fill_charges($connection)
                                             {
@@ -224,6 +285,126 @@ include("ajaxcallx.php");
                                                     });
                                                 })
                                             </script>
+
+                                            <!-- script for autocomplete dropdown -->
+                                            <script>
+                                                $(document).ready(function (){
+                                                    function autocomplete(inp, arr) {
+                                                        /*the autocomplete function takes two arguments,
+                                                        the text field element and an array of possible autocompleted values:*/
+                                                        var currentFocus;
+                                                        /*execute a function when someone writes in the text field:*/
+                                                        inp.addEventListener("input", function(e) {
+                                                            var a, b, i, val = this.value;
+                                                            /*close any already open lists of autocompleted values*/
+                                                            closeAllLists();
+                                                            if (!val) { return false;}
+                                                            currentFocus = -1;
+                                                            /*create a DIV element that will contain the items (values):*/
+                                                            a = document.createElement("DIV");
+                                                            a.setAttribute("id", this.id + "autocomplete-list");
+                                                            a.setAttribute("class", "autocomplete-items");
+                                                            /*append the DIV element as a child of the autocomplete container:*/
+                                                            this.parentNode.appendChild(a);
+                                                            /*for each item in the array...*/
+                                                            for (i = 0; i < arr.length; i++) {
+                                                                /*check if the item starts with the same letters as the text field value:*/
+                                                                if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                                                                /*create a DIV element for each matching element:*/
+                                                                b = document.createElement("DIV");
+                                                                /*make the matching letters bold:*/
+                                                                b.innerHTML =  arr[i].substr(0, val.length);
+                                                                b.innerHTML += arr[i].substr(val.length);
+                                                                /*insert a input field that will hold the current array item's value:*/
+                                                                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                                                                /*execute a function when someone clicks on the item value (DIV element):*/
+                                                                b.addEventListener("click", function(e) {
+                                                                    /*insert the value for the autocomplete text field:*/
+                                                                    inp.value = this.getElementsByTagName("input")[0].value;
+                                                                    /*close the list of autocompleted values,
+                                                                    (or any other open lists of autocompleted values:*/
+                                                                    closeAllLists();
+                                                                });
+                                                                a.appendChild(b);
+                                                                }
+                                                            }
+                                                        });
+                                                        /*execute a function presses a key on the keyboard:*/
+                                                        inp.addEventListener("keydown", function(e) {
+                                                            var x = document.getElementById(this.id + "autocomplete-list");
+                                                            if (x) x = x.getElementsByTagName("div");
+                                                            if (e.keyCode == 40) {
+                                                                /*If the arrow DOWN key is pressed,
+                                                                increase the currentFocus variable:*/
+                                                                currentFocus++;
+                                                                /*and and make the current item more visible:*/
+                                                                addActive(x);
+                                                            } else if (e.keyCode == 38) { //up
+                                                                /*If the arrow UP key is pressed,
+                                                                decrease the currentFocus variable:*/
+                                                                currentFocus--;
+                                                                /*and and make the current item more visible:*/
+                                                                addActive(x);
+                                                            } else if (e.keyCode == 13) {
+                                                                /*If the ENTER key is pressed, prevent the form from being submitted,*/
+                                                                e.preventDefault();
+                                                                if (currentFocus > -1) {
+                                                                /*and simulate a click on the "active" item:*/
+                                                                if (x) x[currentFocus].click();
+                                                                }
+                                                            }
+                                                        });
+                                                        function addActive(x) {
+                                                            /*a function to classify an item as "active":*/
+                                                            if (!x) return false;
+                                                            /*start by removing the "active" class on all items:*/
+                                                            removeActive(x);
+                                                            if (currentFocus >= x.length) currentFocus = 0;
+                                                            if (currentFocus < 0) currentFocus = (x.length - 1);
+                                                            /*add class "autocomplete-active":*/
+                                                            x[currentFocus].classList.add("autocomplete-active");
+                                                        }
+                                                        function removeActive(x) {
+                                                            /*a function to remove the "active" class from all autocomplete items:*/
+                                                            for (var i = 0; i < x.length; i++) {
+                                                            x[i].classList.remove("autocomplete-active");
+                                                            }
+                                                        }
+                                                        function closeAllLists(elmnt) {
+                                                            /*close all autocomplete lists in the document,
+                                                            except the one passed as an argument:*/
+                                                            var x = document.getElementsByClassName("autocomplete-items");
+                                                            for (var i = 0; i < x.length; i++) {
+                                                            if (elmnt != x[i] && elmnt != inp) {
+                                                                x[i].parentNode.removeChild(x[i]);
+                                                            }
+                                                            }
+                                                        }
+                                                        /*execute a function when someone clicks in the document:*/
+                                                        document.addEventListener("click", function (e) {
+                                                            closeAllLists(e.target);
+                                                        });
+                                                        }
+
+                                                        var client_info = <?php echo json_encode(fill_client($connection)); ?>;
+[]
+                                                        var clients = client_info.split(",");
+                                                        let  clientNames =   clients.map((x) =>{
+                                                                return   x.split("-")[1]
+                                                                })
+                                                        let  clientIds =   clients.map((x) =>{
+                                                                return   x.split("-")[0]
+                                                                })
+                                                        console.log(clientIds, clientNames)
+                                                        
+                                                        /*An array containing all the country names in the world:*/
+
+                                                        /*initiate the autocomplete function on the "client_name" element, and pass along the clients array as possible autocomplete values:*/
+                                                        autocomplete(document.getElementById("client_name"), clientNames);
+                                                })
+
+                                    
+                                            </script>
                                             <script>
                                                 $(document).ready(function () {
                                                     $('#client_name').change(function () {
@@ -243,10 +424,15 @@ include("ajaxcallx.php");
                                             <div class="row">
                                                 <div class="col-md-4">
                                                     <label class="bmd-label-floating">Client Name *:</label>
-                                                    <select name="client_id" class="form-control" id="client_name">
+                                                    <!-- <select name="client_id" class="form-control" id="client_name">
                                                         <option value="">select an option</option>
-                                                        <?php echo fill_client($connection); ?>
-                                                    </select>
+                                                      
+                                                    </select> -->
+
+                                                    <!-- autocomplete dropdown for client names -->
+                                                    <div class="autocomplete" style="width:300px;">
+                                                         <input id="client_name" type="text" name="clientName" placeholder="">
+                                                    </div>
                                                 </div>
                                                 <div class="col-md-4">
                                                     <label class="bmd-label-floating">Product *:</label>
