6c9cb0f2d5f538130a2cc2a081271cb687e82d3e
Nylat119 <36956517+Nylat119@users.noreply.github.com>
2021-03-25 11:45:17 +0100

    chq book posting, statement of FP, statement of income, portfolio

--- functions/DbModel/db.php
+++ functions/DbModel/db.php
@@ -3,7 +3,7 @@
 server with default setting (user 'root' with no pssword) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'sekanisy');
-define('DB_PASSWORD', '4r6WY#JP+rnl67');
+define('DB_PASSWORD', '4rWY#JP+rnl67');
 define('DB_CHARSET', 'utf-8');
 define('DB_NAME', 'sekanisy_admin');
 // connect to the database with the defined values
