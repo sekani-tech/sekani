e71280643f04559e24b5e44f95d219138b1b66c9
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2021-04-21 17:42:45 +0100

    Matching Transactions UI

--- mfi/trans_match.php
+++ mfi/trans_match.php
@@ -24,24 +24,47 @@ include("header.php");
 
         <div class="row">
             <div class="col-md-12 ml-auto mr-auto">
-                <div class="card card-pricing bg-primary">
+                <div class="card card-pricing">
                     <div class="card-header card-header-primary">
-                        <h4 class="card-title">All Transactions</h4>
+                        <h4 class="card-title">Matching Transactions</h4>
                         <!-- <p class="category">Category subtitle</p> -->
                     </div>
                     <div class="card-body ">
                         <div class="row">
-                            <div class="col-md-6">
-                                <h3 class="card-title">Transaction ID: </h3>
-                                <h3 class="card-title">Amount: </h3>
-                                <h3 class="card-title">Transaction Date: </h3>
-                                <h3 class="card-title">Deposited by: </h3>
-                            </div>
-                            <div class="col-md-6">
-                                <h3 class="card-title">Transaction ID: </h3>
-                                <h3 class="card-title">Amount: </h3>
-                                <h3 class="card-title">Transaction Date: </h3>
-                                <h3 class="card-title">Deposited by: </h3>
+                            <div class="col-md-12">
+                                <table id="example" class="table table-striped table-bordered" style="width:100%">
+                                    <thead>
+                                        <tr>
+                                            <th>Transaction ID</th>
+                                            <th>Amount</th>
+                                            <th>Transaction Date</th>
+                                            <th>Description</th>
+                                            <th>Amount</th>
+                                            <th>Transaction Date</th>
+                                            <th>Description</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><b>Transaction ID - </b>10234</td>
+                                            <td>N30,000</td>
+                                            <td>12/04/2021</td>
+                                            <td>Fuel</td>
+                                            <td>N30,000</td>
+                                            <td>12/04/2021</td>
+                                            <td>Fuel</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Transaction ID - </b>10214</td>
+                                            <td>N34,000</td>
+                                            <td>12/04/2021</td>
+                                            <td>Fuel</td>
+                                            <td>N34,000</td>
+                                            <td>12/04/2021</td>
+                                            <td>Fuel</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
 
                             </div>
                         </div>
@@ -56,32 +79,72 @@ include("header.php");
 
         <div class="row">
             <div class="col-md-6 ml-auto mr-auto">
-                <div class="card card-pricing bg-success">
+                <div class="card card-pricing">
                     <div class="card-header card-header-success">
-                        <h4 class="card-title">Found</h4>
+                        <h4 class="card-title">Sekani (Not Uploaded)</h4>
                         <!-- <p class="category">Category subtitle</p> -->
+                        <div style="float: right;">
+                            <button type="button" class="btn btn-info">Discard</button>
+                        </div>
                     </div>
                     <div class="card-body">
-                        <h3 class="card-title">Transaction ID: </h3>
-                        <h3 class="card-title">Amount: </h3>
-                        <h3 class="card-title">Transaction Date: </h3>
-                        <h3 class="card-title">Deposited by: </h3>
+                        <div class="col-md-12">
+                            <table id="id1" class="table table-striped table-bordered" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>Transaction ID</th>
+                                        <th>Amount</th>
+                                        <th>Transaction Date</th>
+                                        <th>Description</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>10234</td>
+                                        <td>N30,000</td>
+                                        <td>12/04/2021</td>
+                                        <td>Fuel</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
 
             </div>
 
             <div class="col-md-6 ml-auto mr-auto">
-                <div class="card card-pricing bg-danger">
+                <div class="card card-pricing">
                     <div class="card-header card-header-danger">
-                        <h4 class="card-title">Not Found</h4>
+                        <h4 class="card-title">Uploaded Not Found</h4>
                         <!-- <p class="category">Category subtitle</p> -->
+                        <div style="float: right;">
+                            <button type="button" class="btn btn-info">Discard</button>
+                            <button type="button" class="btn btn-info">Upload</button>
+                        </div>
                     </div>
                     <div class="card-body ">
-                        <h3 class="card-title">Transaction ID: </h3>
-                        <h3 class="card-title">Amount: </h3>
-                        <h3 class="card-title">Transaction Date: </h3>
-                        <h3 class="card-title">Deposited by: </h3>
+                        <table id="id2" class="table table-striped table-bordered" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>Transaction ID</th>
+                                    <th>Amount</th>
+                                    <th>Transaction Date</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>10234</td>
+                                    <td>N30,000</td>
+                                    <td>12/04/2021</td>
+                                    <td>Fuel</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -91,6 +154,113 @@ include("header.php");
     </div>
 
 </div>
+<script>
+    $(document).ready(function() {
+        var groupColumn = 0;
+        var table = $('#example').DataTable({
+            select: true,
+            "columnDefs": [{
+                "visible": false,
+                "targets": groupColumn
+                
+            }],
+            "order": [
+                [groupColumn, 'asc']
+            ],
+            "displayLength": 25,
+            "drawCallback": function(settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+
+                api.column(groupColumn, {
+                    page: 'current'
+                }).data().each(function(group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before(
+                            '<tr class="group"><td colspan="5">' + group + '</td></tr>'
+                        );
+
+                        last = group;
+                    }
+                });
+            }
+        });
+
+        var table1 = $('#id1').DataTable({
+            "columnDefs": [{
+                "visible": false,
+                "targets": groupColumn
+            }],
+            "order": [
+                [groupColumn, 'asc']
+            ],
+            "displayLength": 25,
+            "drawCallback": function(settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+
+                api.column(groupColumn, {
+                    page: 'current'
+                }).data().each(function(group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before(
+                            '<tr class="group"><td colspan="5">' + group + '</td></tr>'
+                        );
+
+                        last = group;
+                    }
+                });
+            }
+        });
+
+        var table = $('#id2').DataTable({
+            "columnDefs": [{
+                "visible": false,
+                "targets": groupColumn
+            }],
+            "order": [
+                [groupColumn, 'asc']
+            ],
+            "displayLength": 25,
+            "drawCallback": function(settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+
+                api.column(groupColumn, {
+                    page: 'current'
+                }).data().each(function(group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before(
+                            '<tr class="group"><td colspan="5">' + group + '</td></tr>'
+                        );
+
+                        last = group;
+                    }
+                });
+            }
+        });
+        
+
+        // Order by the grouping
+        $('#example tbody').on('click', 'tr.group', function() {
+            var currentOrder = table.order()[0];
+            if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+                table.order([groupColumn, 'desc']).draw();
+            } else {
+                table.order([groupColumn, 'asc']).draw();
+            }
+        });
+    });
+</script>
 
 
 <?php
