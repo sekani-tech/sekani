39d862b37c85a800968adab2405d10cc9353f3a3
Terver Ugbe <32528384+Dennisruso@users.noreply.github.com>
2021-04-22 14:34:43 +0100

    FTD Product Validating Form

--- mfi/manage_fd_product.php
+++ mfi/manage_fd_product.php
@@ -627,7 +627,7 @@ $sint_id = $_SESSION['int_id'];
                     // add an "invalid" class to the field:
                     y[i].className += " invalid";
                     // and set the current valid status to false
-                    valid = false; // This was change to true to disable the validation function. Should be reverted to FALSE after testing is complete
+                    valid = true; // This was change to true to disable the validation function. Should be reverted to FALSE after testing is complete
                 }
             }
             // If the valid status is true, mark the step as finished and valid:
